7d07f0eea1e780f5c9afcc413bc5df2c857b8480
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-01-24 23:14:24 +0530

    isomorphic string

--- basic-string/string.cpp
+++ basic-string/string.cpp
@@ -62,6 +62,20 @@ string longestCommonPrefix(vector<string>& str){
     return ans;
 }
 
+bool isomorphicString(string s, string t) {
+    int map_s[256] = {0};
+    int map_t[256] = {0};
+
+    int n = s.size();
+
+    for(int i=0;i<n;i++) {
+        if(map_s[s[i]] != map_t[t[i]]) return false;
+        map_s[s[i]] = i+1;
+        map_t[t[i]] = i+1;
+    }
+    return true;
+}
+
 // O(N * log N + M), where N is the number of strings and 
 // M is the maximum length of a string. The sorting operation 
 // takes O(N * log N) time, and the 
