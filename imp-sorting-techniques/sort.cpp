8da49d3ed9033b9341623d8a2c1f30f703af95a1
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-01-29 01:54:42 +0530

    bubble sort

--- imp-sorting-techniques/sort.cpp
+++ imp-sorting-techniques/sort.cpp
@@ -16,6 +16,25 @@ vector<int> selectionSort(vector<int>& nums) {
     }
     return nums;
 }
+
+// push the max to the last by adjacent swaps
+vector<int> bubbleSort(vector<int>& nums) {
+    int n = nums.size();
+    int didSwap = 0;
+    for(int i = n-1;i >= 0;i--) {
+        for(int j = 0; j < i;j++) {
+            if(nums[j] > nums[j+1]) {
+                swap(nums[j],nums[j+1]);
+                // if swap happended at any time
+                didSwap = 1;
+            }
+        }
+        // if no swaps happened out of the loop as 
+        // it is already sorted
+        if(didSwap == 0) break;
+    }
+    return nums;
+}
 int main() {
     ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
