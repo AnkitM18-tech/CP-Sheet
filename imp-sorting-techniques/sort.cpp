7b9f13f003a833b9310debd6dcf3d3454b38ed03
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-01-30 02:00:25 +0530

    Merge Sort

--- imp-sorting-techniques/sort.cpp
+++ imp-sorting-techniques/sort.cpp
@@ -51,6 +51,50 @@ vector<int> insertionSort(vector<int>& nums) {
 // Worst - Avg - O(N^2)
 // Best - O(N)
 
+void merge(vector<int>& nums, int low, int mid, int high) {
+    vector<int> temp;
+    int left = low;
+    int right = mid + 1;
+
+    while(left <= mid && right <= high) {
+        if(nums[left] <= nums[right]) {
+            temp.push_back(nums[left]);
+            left++;
+        } else {
+            temp.push_back(nums[right]);
+            right++;
+        }
+    }
+
+    while(left <= mid) {
+        temp.push_back(nums[left]);
+        left++;
+    }
+
+    while(right <= high) {
+        temp.push_back(nums[right]);
+        right++;
+    }
+
+    for(int i=low;i<=high;i++) {
+        nums[i] = temp[i-low];
+    }
+}
+
+void mergeSortHelper(vector<int>& nums, int low, int high) {
+    if(low >= high) return;
+    int mid = (low + high)/2;
+    mergeSortHelper(nums,low,mid);
+    mergeSortHelper(nums,mid+1,high);
+    merge(nums,low,mid,high);
+}
+
+// Divide and Merge
+vector<int> mergeSort(vector<int>& nums) {
+    int n = nums.size();
+    mergeSortHelper(nums,0,n-1);
+    return nums;
+} // TC - O(nlogn) , SC - O(n)
 int main() {
     ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
