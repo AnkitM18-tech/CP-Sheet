d05cf71038ceb1cf286d44b140f4cf98e447d99c
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-02-02 01:32:11 +0530

    rotateArrayLeft by one position

--- array_medium.cpp
+++ array_medium.cpp
@@ -79,6 +79,15 @@ int findMaxConsecutiveOnes(vector<int>& nums) {
     }
     return maxCnt;
 }
+
+void rotateArrayByOne(vector<int>& nums) {
+    int temp = nums[0];
+
+    for(int i = 1; i< nums.size();i++) {
+        nums[i-1] = nums[i];
+    }
+    nums[nums.size()-1] = temp;
+}
 int main() {
     ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
