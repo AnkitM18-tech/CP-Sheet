00d7c468604595571df01c33694c5a661bba2d42
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-02-05 02:28:10 +0530

    spiral order

--- arrays/array-fundamental.cpp
+++ arrays/array-fundamental.cpp
@@ -240,6 +240,43 @@ vector<int> leaders(vector<int>& nums) {
     return ans;
 }
 
+vector<int> spiralOrder(vector<vector<int>>& matrix) {
+    int m = matrix.size(); // row
+    int n = matrix[0].size(); // column
+    int left = 0, right = n - 1;
+    int top = 0, bottom = m - 1;
+    vector<int> ans;
+
+    while(top <= bottom && left <= right) {
+        // right
+        for(int i = left; i<=right; i++) {
+            ans.push_back(matrix[top][i]);
+        }
+        top++;
+        // bottom
+        for(int i = top; i <= bottom; i++) {
+            ans.push_back(matrix[i][right]);
+        }
+        right--;
+        // left
+        if(top <= bottom) {
+            for(int i = right; i>=left; i--) {
+                ans.push_back(matrix[bottom][i]);
+            }
+            bottom--;
+        }
+        // top
+        if(left <= right) {
+            for(int i = bottom; i>=top; i--) {
+                ans.push_back(matrix[i][left]);
+            }
+            left++;
+        }
+    }
+    return ans; // TC - O(MxN)
+}
+
+
 
 int main() {
     ios_base::sync_with_stdio(false);
