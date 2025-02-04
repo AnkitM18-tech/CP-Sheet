1bd8e19d207baae0de5de65dfc2657c8c0448f3b
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-02-04 20:12:41 +0530

    intersection of sorted arrays

--- arrays/array-fundamental.cpp
+++ arrays/array-fundamental.cpp
@@ -172,6 +172,38 @@ vector<int> unionArray(vector<int>& nums1, vector<int>& nums2) {
     return Union;
 }
 
+vector<int> intersectionArray(vector<int>& nums1, vector<int>& nums2) {
+    vector<int> ans;
+    /* // Brute
+    vector<int> visited(nums2.size(),0);
+    for(int i = 0; i < nums1.size(); i++) {
+        for(int j = 0; j < nums2.size(); j++) {
+            if(nums1[i] == nums2[j] && visited[j] == 0) {
+                ans.push_back(nums2[j]);
+                visited[j] = 1;
+                break;
+            } else if(nums2[j] > nums1[i]) break;
+        }
+    }
+    return ans; // O(MxN)
+    */
+    // Optimal
+    int i = 0, j = 0;
+    int n = nums1.size(), m = nums2.size();
+    while(i < n && j < m) {
+        if(nums1[i] < nums2[j]) {
+            i++;
+        } else if(nums2[j] < nums1[i]) {
+            j++;
+        } else {
+            ans.push_back(nums1[i]);
+            i++; j++;
+        }
+    }
+    return ans;
+    // O(M), 
+    // where M is the length of that array which has less elements.
+}
 int main() {
     ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
