5b979c5b9f01dfffb835b133bd3adff0077c7299
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-02-11 02:05:57 +0530

    3 sum

--- arrays/array-medium-2.cpp
+++ arrays/array-medium-2.cpp
@@ -60,7 +60,74 @@ vector<int> twoSum(vector<int>& nums, int target) {
     // the space complexity will be O(N).
 }
 
+vector<vector<int>> threeSum(vector<int>& nums) {
+    int n = nums.size();
+    /*
+    // Brute
+    set<vector<int>> ans;
+    for(int i = 0; i < n - 2; i++) {
+        for(int j = i + 1; j < n-1; j++) {
+            for(int k = j + 1; k < n; k++) {
+                if(nums[i] + nums[j] + nums[k] == 0) {
+                    vector<int> temp = {nums[i],nums[j],nums[k]};
+                    sort(temp.begin(),temp.end());
+                    ans.insert(temp);
+                }
+            }
+        }
+    }
+    vector<vector<int>> triplet(ans.begin(),ans.end());
+    return triplet;
+    // SC - O(2 x no. of the unique triplets)
+    // TC - O(N^3 x log(no. of unique triplets))
+    
+    // Better
+    set<vector<int>> triplet;
+    for(int i = 0; i<n; i++) {
+        set<int> hashset;
+        for(int j = i + 1; j<n;j++) {
+            int third = -(nums[i] + nums[j]);
+            if(hashset.find(third) != hashset.end()) {
+                vector<int> temp = {nums[i],nums[j],third};
+                sort(temp.begin(),temp.end());
+                triplet.insert(temp);
+            }
+            hashset.insert(nums[j]);
+        }
+    }
+    vector<vector<int>> ans(triplet.begin(),triplet.end());
+    return ans;
+    // SC - O(2 x no. of the unique triplets) + O(N)
+    // TC - O(N^2 x log(no. of unique triplets))
+    */
+    vector<vector<int>> ans;
+    sort(nums.begin(),nums.end());
 
+    for(int i = 0; i<n;i++) {
+        if(i > 0 && nums[i] == nums[i-1]) continue;
+
+        int j = i+1;
+        int k = n-1;
+
+        while(j < k) {
+            int sum = nums[i] + nums[j] + nums[k];
+            if(sum < 0) {
+                j++;
+            } else if( sum > 0) {
+                k--;
+            } else {
+                vector<int> temp = {nums[i],nums[j],nums[k]};
+                ans.push_back(temp);
+                j++;
+                k--;
+                while(j < k && nums[j] == nums[j-1]) j++;
+                while(j < k && nums[k] == nums[k+1]) k--;
+            }
+        }
+    }
+    return ans;
+    // TC - O(NlogN)+O(N^2)
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
