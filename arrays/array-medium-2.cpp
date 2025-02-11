4e2d5031665e9c9f4ab29aca1124a1da15cac2ba
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-02-12 00:43:28 +0530

    4 sum

--- arrays/array-medium-2.cpp
+++ arrays/array-medium-2.cpp
@@ -129,6 +129,85 @@ vector<vector<int>> threeSum(vector<int>& nums) {
     // TC - O(NlogN)+O(N^2)
 }
 
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    int n = nums.size();
+    /*
+    set<vector<int>> st;
+    // Brute
+    for(int i = 0; i<n; i++) {
+        for(int j = i+1; j<n; j++) {
+            for(int k = j+1; k<n; k++) {
+                for(int l = k+1; l<n; l++) {
+                    if(nums[i] + nums[j] + nums[k] + nums[l] == target) {
+                        vector<int> temp = {nums[i],nums[j],nums[k],nums[l]};
+                        sort(temp.begin(),temp.end());
+                        st.insert(temp);
+                    }
+                }
+            }
+        }
+    }
+
+    vector<vector<int>> quad(st.begin(),st.end());
+    return quad; // TC -  O(N^4), SC - O(2 x no. of the quadruplets)
+    
+    // Better
+    for(int i = 0; i<n; i++) {
+        for(int j = i+1; j<n; j++) {
+            set<long long> hashset;
+            for(int k = j+1;k<n; k++) {
+                long long sum = nums[i] + nums[j] + nums[k];
+                long long fourth = target - sum;
+
+                if(hashset.find(fourth) != hashset.end()) {
+                    vector<int> temp = {nums[i],nums[j],nums[k],fourth};
+                    sort(temp.begin(),temp.end());
+                    st.insert(temp);
+                }
+                hashset.insert(nums[k]);
+            }
+        }
+    }
+    vector<vector<int>> quad(st.begin(),st.end());
+    return quad; // TC - O(N^3xlog(M)), SC - O(2 x no. of the quadruplets)+O(N)
+    */
+    // Optimal
+    vector<vector<int>> quad;
+    sort(nums.begin(),nums.end());
+
+    for(int i = 0; i<n; i++) {
+        if(i > 0 && nums[i] == nums[i-1]) {
+            continue;
+        }
+        for(int j = i+1; j<n; j++) {
+            if(j > i + 1 && nums[j] == nums[j-1]) continue;
+
+            int left = j + 1, right = n - 1;
+
+            while(left < right) {
+                long long sum = nums[i] + nums[j] + nums[left] + nums[right];
+                if(sum == target) {
+                    vector<int> temp = {nums[i], nums[j], nums[left], nums[right]};
+                    quad.push_back(temp);
+                    left++;
+                    right--;
+
+                    while(left < right && nums[left] == nums[left-1]) left++;
+                    while(left < right && nums[right] == nums[right+1]) right--;
+                } else if(sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+    }
+    return quad;
+    // TC - O(N^3), SC - O(no. of quadruplets)
+}
+
+
+
 int main() {
     ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
