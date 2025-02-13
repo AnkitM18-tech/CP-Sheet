6c82c6fa0f275ec4182a7adc4147bb127524642a
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-02-13 23:53:00 +0530

    max subarray - kadane algorithm

--- arrays/array-medium-2.cpp
+++ arrays/array-medium-2.cpp
@@ -249,6 +249,44 @@ void sortZeroOneTwo(vector<int>& nums) {
     // Index high +1 to sizeOfArray - 1 contains 2.
 }
 
+int maxSubArray(vector<int>& nums) {
+    int n = nums.size();
+    int maxi = INT_MIN;
+    /*
+    // Brute
+    for(int i = 0; i < n; i++) {
+        for(int j = i; j<n;j++) {
+            int sum = 0;
+            for(int k = i; k<=j; k++) {
+                sum += nums[k];
+            }
+            maxi = max(maxi,sum);
+        }
+    }
+    return maxi; // O(N^3)
+    
+    // Better
+    for(int i = 0; i<n; i++) {
+        int sum = 0;
+        for(int j = i; j<n; j++) {
+            sum += nums[j];
+            maxi = max(maxi,sum);
+        }
+    }
+    return maxi; // O(N^2)
+    */
+    // Optimal
+    int sum = 0;
+    for(int i = 0; i<n; i++) {
+        sum += nums[i];
+        if(sum > maxi) {
+            maxi = sum;
+        }
+        if(sum < 0) sum = 0;
+    }
+    return maxi;
+}
+
 
 
 int main() {
