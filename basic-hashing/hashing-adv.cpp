c248d726a8398fb891e89d1f218768ab7025e8c1
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-02-16 03:26:17 +0530

    longest consecutive sequence optimal

--- basic-hashing/hashing-adv.cpp
+++ basic-hashing/hashing-adv.cpp
@@ -47,6 +47,28 @@ int longestConsecutive(vector<int>& nums) {
     }
     return longest; //  O(NlogN) + O(N)
     */
+   // Optimal
+    int longest = 1;
+    unordered_set<int> st;
+
+    for(int i = 0; i<n; i++) {
+        st.insert(nums[i]);
+    }
+    
+    for(auto it : st) {
+        if(st.find(it - 1) == st.end()) {
+            int cnt = 1;
+            int x = it;
+
+            while(st.find(x+1) != st.end()) {
+                x += 1;
+                cnt += 1;
+            }
+
+            longest = max(longest,cnt);
+        }
+    }
+    return longest; // O(N) + O(2xN) ~ O(3xN)
 }
 
 int main() {
