23921761a96f9bac5c564952a484801d4a90b15e
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-02-17 23:46:52 +0530

    longest subarray

--- basic-hashing/hashing-adv.cpp
+++ basic-hashing/hashing-adv.cpp
@@ -71,6 +71,66 @@ int longestConsecutive(vector<int>& nums) {
     return longest; // O(N) + O(2xN) ~ O(3xN)
 }
 
+int longestSubarray(vector<int> &nums, int k){
+    int n = nums.size();
+    /*
+    // Brute
+    if(n == 0) return 0;
+    int longest = 0;
+    for(int i = 0; i<n; i++) {
+        int sum = 0;
+        for(int j = i; j<n; j++) {
+            sum += nums[j];
+            if(sum == k) {
+                longest = max(j-i+1,longest);
+                break;
+            } else if(sum > k) {
+                break;
+            }
+        }
+    }
+    return longest; // O(N^2)
+
+    // Optimal - for positives
+    int left = 0, right = 0;
+    long long sum = nums[0];
+    int maxLen = 0;
+    while(right < n) {
+        while(left <= right && sum > k) {
+            sum -= nums[left];
+            left++;
+        }
+        if(sum == k) {
+            maxLen = max(maxLen, right - left + 1);
+        }
+        right++;
+        if(right < n) sum += nums[right];
+    }
+    return maxLen; // O(N)
+    */
+    // Better - Only Positives , Optimal - for positives + negatives
+    map<long long, int> prefixSum;
+    long long sum = 0;
+    int maxLen = 0;
+    for(int i = 0; i<n; i++) {
+        sum += nums[i];
+        if(sum == k) {
+            maxLen = max(maxLen, i+1);
+        }
+        long long rem = sum - k;
+        if(prefixSum.find(rem) != prefixSum.end()) {
+            int len = i - prefixSum[rem];
+            maxLen = max(maxLen,len);
+        }
+        if(prefixSum.find(sum) == prefixSum.end()) {
+            prefixSum[sum] = i;
+        }
+    }
+
+    return maxLen; // TC - O(NlogN), SC - O(N)
+}
+
+
 int main() {
     ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
