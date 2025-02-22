f1828705d36c960430deef286edb95576ad9e7f0
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-02-22 17:42:27 +0530

    upper bound

--- binary-search/binary-fundamentals.cpp
+++ binary-search/binary-fundamentals.cpp
@@ -64,6 +64,32 @@ int lowerBound(vector<int> &nums, int x){
     return ans; // O(log N)
 }
 
+int upperBound(vector<int> &nums, int x){
+    int n = nums.size();
+    /*
+    // Brute
+    for(int i = 0; i<n; i++) {
+        if(nums[i] > x) {
+            return i;
+        }
+    }
+    return n; // O(N)
+    */
+    // Optimal
+    int low = 0, high = n-1;
+    int ans = n;
+    while(low <= high) {
+        int mid = (low + high)/2;
+        if(nums[mid] > x) {
+            ans = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return ans; // O(log N)
+}
+
 
 int main() {
     ios_base::sync_with_stdio(false);
