96c049bce97ecdd16a8929ceebb07156e213dc84
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-02-24 00:03:21 +0530

    first and last occurence & search in rotated array

--- binary-search/binary-search-logic.cpp
+++ binary-search/binary-search-logic.cpp
@@ -88,6 +88,130 @@ vector<int> getFloorAndCeil(vector<int> nums, int x) {
     return {floor,ceil}; // O(log N)
 }
 
+vector<int> linearSearch(vector<int> &nums, int target) {
+    int n = nums.size();
+    int start = -1, end = -1;
+    for(int i = 0; i<n; i++) {
+        if(nums[i] == target) {
+            if(start == -1) start = i;
+            end = i;
+        }
+    }
+    return {start,end}; // O(N)
+}
+int lowerBound(vector<int> &nums, int target) {
+    int n = nums.size();
+    int low = 0, high = n-1, ans = n;
+    while(low <= high) {
+        int mid = (low + high)/2;
+        if(nums[mid] >= target) {
+            ans = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return ans; // O(log N)
+}
+int upperBound(vector<int> &nums, int target) {
+    int n = nums.size();
+    int low = 0, high = n-1, ans = n;
+    while(low <= high) {
+        int mid = (low + high)/2;
+        if(nums[mid] > target) {
+            ans = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return ans; // O(log N)
+}
+int first(vector<int> &nums, int target) {
+    int n = nums.size(), ans = -1;
+    int low = 0, high = n-1;
+    while(low <= high) {
+        int mid = (low + high)/2;
+        if(nums[mid] == target) {
+            ans = mid;
+            high = mid - 1;
+        } else if(nums[mid] < target){
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
+int last(vector<int> &nums, int target) {
+    int n = nums.size(), ans = -1;
+    int low = 0, high = n-1;
+    while(low <= high) {
+        int mid = (low + high)/2;
+        if(nums[mid] == target) {
+            ans = mid;
+            low = mid + 1;
+        } else if(nums[mid] < target){
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
+
+vector<int> searchRange(vector<int> &nums, int target) {
+    // Linear Search - O(N)
+    // return linearSearch(nums,target);
+    /*
+    // Using Bounds - 2*O(log N)
+    int start = lowerBound(nums,target);
+    if(start == nums.size() || nums[start] != target) return {-1,-1};
+    int end = upperBound(nums,target) - 1;
+    return {start,end};
+    */
+    // Binary Search - O(log N)
+    int start = first(nums,target);
+    if(start == -1) return {-1,-1};
+    int end = last(nums,target);
+    return {start,end};
+}
+
+int linearSearch2(vector<int> &nums, int target) {
+    int n = nums.size();
+    for(int i = 0; i<n; i++) {
+        if(nums[i] == target) {
+            return i;
+        }
+    }
+    return -1; // O(N)
+}
+
+int search(vector<int> &nums, int k) {
+    int n = nums.size();
+    int low = 0, high = n - 1;
+    while(low <= high) {
+    int mid = (low + high)/2;
+    if(nums[mid] == k) {
+        return mid;
+    }
+    if(nums[low] <= nums[mid]) {
+        if(nums[low] <= k && k <= nums[mid]) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    } else {
+        if(nums[mid] <= k && k <= nums[high]) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    }
+    return -1; // O(log N)
+}
+
 
 int main() {
     ios_base::sync_with_stdio(false);
