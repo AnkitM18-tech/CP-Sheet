1f8000d47533b0741ac46f6f9d792125048c3b9d
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-02-28 01:41:00 +0530

    rose garden

--- binary-search/binary-search-on-answers.cpp
+++ binary-search/binary-search-on-answers.cpp
@@ -198,6 +198,70 @@ int minimumRateToEatBananas(vector<int> &nums, int h) {
     // maximum element in the array and N is size of the array
 }
 
+ bool possible(vector<int> &nums, int day, int m, int k) {
+    int n = nums.size();
+    int cnt = 0, noOfBqt = 0;
+    for(int i = 0; i<n; i++) {
+        if(nums[i] <= day) {
+            cnt++;
+        } else {
+            noOfBqt += (cnt/k);
+            cnt = 0;
+        }
+    }
+    noOfBqt += (cnt/k);
+
+    return noOfBqt >= m;
+}
+
+int linearRoseGarden(int n, vector<int> &nums, int k, int m) {
+    long long val = m * 1ll * k * 1ll;
+
+    /* Impossible case: not enough 
+    flowers to make m bouquets*/
+    if(val > n) return -1;
+
+    int mini = INT_MAX, maxi = INT_MIN;
+    for (int i = 0; i < n; i++) {
+        mini = min(mini, nums[i]); 
+        maxi = max(maxi, nums[i]); 
+    }
+
+    for(int i = mini; i <= maxi; i++) {
+        if(possible(nums,i,m,k)) {
+            return i;
+        }
+    }
+    return -1;
+    // O((max-min+1) * N), where max is the maximum element of the array, 
+    // min is the minimum element of the array, N is size of the array}.
+}
+
+int roseGarden(int n,vector<int> &nums, int k, int m) {
+    long long val = m * 1ll * k * 1ll;
+    /* Impossible case: not enough 
+    flowers to make m bouquets*/
+    if(val > n) return -1;
+    int mini = INT_MAX, maxi = INT_MIN;
+    for (int i = 0; i < n; i++) {
+        mini = min(mini, nums[i]); 
+        maxi = max(maxi, nums[i]); 
+    }
+    int low = mini, high = maxi, ans = -1;
+    while(low <= high) {
+        int mid = low + (high - low)/2; // eq - low + high
+        if(possible(nums,mid,m,k)) {
+            ans = mid;
+            high = mid -1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return ans;
+    // O(log(max-min+1) * N), where max is the maximum element of the array, 
+    // min is the minimum element of the array, N is size of the array.
+}
+
 
 int main() {
     ios_base::sync_with_stdio(false);
