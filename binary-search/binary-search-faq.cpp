1ad78b8e8b4a40c198a458fb6ed0ab7e7f133b35
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-03-01 19:54:04 +0530

    peak element

--- binary-search/binary-search-faq.cpp
+++ binary-search/binary-search-faq.cpp
@@ -100,6 +100,35 @@ int findPages(vector<int> &nums, int m)  {
     // max is the maximum of all array elements.
 }
 
+int findPeakElement(vector<int> &arr) {
+    int n = arr.size();
+    /*
+    // Linear Search
+    for(int i = 0; i<n; i++) {
+    if((i == 0 || arr[i-1] < arr[i]) && (i == n - 1 || arr[i] > arr[i+1])) {
+        return i;
+    }
+    }
+    return -1; // O( N)
+    */
+    // Binary Search
+    // Edge Cases
+    if(n == 1) return 0;
+    if(arr[0] > arr[1]) return 0;
+    if(arr[n-1] > arr[n-2]) return n-1;
+
+    int low = 1, high = n - 2;
+    while(low <= high) {
+    int mid = (low + high)/2;
+    if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]) {
+        return mid;
+    }
+    if(arr[mid] > arr[mid-1]) low = mid + 1;
+    else high = mid - 1;
+    }
+    return -1; // O(logN)
+}
+
 
 int main() {
     ios_base::sync_with_stdio(false);
