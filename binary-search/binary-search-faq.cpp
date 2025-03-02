2b9c7207ff9591f33b3d74553ba3ae70df6049f1
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-03-02 16:04:54 +0530

    median of 2 sorted arrays

--- binary-search/binary-search-faq.cpp
+++ binary-search/binary-search-faq.cpp
@@ -129,6 +129,100 @@ int findPeakElement(vector<int> &arr) {
     return -1; // O(logN)
 }
 
+double median(vector<int> &arr1, vector<int> &arr2) {
+    int n1 = arr1.size(), n2 = arr2.size();
+    /*
+    // Brute
+    vector<int> merged;
+    int i = 0, j = 0;
+    while(i < n1 && j < n2) {
+    if(arr1[i] < arr2[j]) merged.push_back(arr1[i++]);
+    else merged.push_back(arr2[j++]);
+    }
+    while(i < n1) merged.push_back(arr1[i++]);
+    while(j < n2) merged.push_back(arr2[j++]);
+
+    int n = n1 + n2;
+    if(n % 2 == 1) {
+    return (double)merged[n/2];
+    }
+
+    double median = ((double)merged[n/2] + (double)merged[(n/2)-1]) / 2.0;
+    return median; // SC & TC - O(N1+N2)
+    
+    // Better
+    int n = n1 + n2;
+    
+    // median calculation
+    int ind2 = n/2;
+    int ind1 = ind2 - 1;
+    int cnt = 0;
+    int ind1element = -1, ind2element = -1;
+
+    int i = 0, j = 0;
+    while(i < n1 && j < n2) {
+    if(arr1[i] < arr2[j]) {
+        if(cnt == ind1) ind1element = arr1[i];
+        if(cnt == ind2) ind2element = arr1[i];
+        cnt++;
+        i++;
+    } else {
+        if(cnt == ind1) ind1element = arr2[j];
+        if(ant == ind2) ind2element = arr2[j];
+        cnt++;
+        j++;
+    }
+    }
+
+    while(i<n1) {
+    if(cnt == ind1) ind1element = arr1[i];
+    if(cnt == ind2) ind2element = arr1[i];
+    cnt++;
+    i++;
+    }
+    while(j<n2) {
+    if(cnt == ind1) ind1element = arr2[j];
+    if(cnt == ind2) ind2element = arr2[j];
+    cnt++;
+    j++;
+    }
+
+    if(n%2 == 1) {
+    return (double)ind2element;
+    }
+
+    return (double)((double)(ind1element + ind2element))/2.0;
+    // TC = O(N1+N2)
+    */
+    // Optimal
+    if(n1 > n2) return median(arr2,arr1);
+    int n = n1 + n2;
+    int left = (n1 + n2 + 1) / 2;
+
+    int low = 0, high = n1;
+    while(low <= high) {
+    int mid1 = (low + high) >> 1;
+    int mid2 = left - mid1;
+
+    int l1 = (mid1 > 0) ? arr1[mid1 - 1] : INT_MIN;
+    int r1 = (mid1 < n1) ? arr1[mid1] : INT_MAX;
+    int l2 = (mid2 > 0) ? arr2[mid2 - 1] : INT_MIN;
+    int r2 = (mid2 < n2) ? arr2[mid2] : INT_MAX;
+
+    if(l1 <= r2 && l2 <= r1) {
+        if(n % 2 == 1) return max(l1,l2);
+        else return (max(l1,l2) + min(r1,r2)) / 2.0;
+    } else if(l1 > r2) {
+        high = mid1 - 1;
+    } else {
+        low = mid1 + 1;
+    }
+    }
+    return 0;
+    // O(log(min(N1,N2))), where N1 and N2 are the sizes of two given arrays. 
+    // As, binary search is being applied on the range [0, min(N1, N2)]
+}
+
 
 int main() {
     ios_base::sync_with_stdio(false);
