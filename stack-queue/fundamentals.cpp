9cd5bef19cdcea7aa2cde35f09ab5f11e950e016
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-03-10 17:20:16 +0530

    balanced paranthesis

--- stack-queue/fundamentals.cpp
+++ stack-queue/fundamentals.cpp
@@ -247,6 +247,35 @@ public:
     }
 };
 
+bool isMatched(char open, char close) {
+    if ((open == '(' && close == ')') || (open == '{' && close == '}') ||
+        (open == '[' && close == ']')) {
+        return true;
+    }
+
+    return false;
+}
+
+bool isValid(string str) {
+    stack<char> st;
+    for (int i = 0; i < str.size(); i++) {
+        if (str[i] == '(' || str[i] == '{' || str[i] == '[') {
+            st.push(str[i]);
+        } else {
+            if (st.empty()) return false;
+
+            char ch = st.top();
+            st.pop();
+
+            if (!isMatched(ch, str[i])) {
+                return false;
+            }
+        }
+    }
+    return st.empty();
+    // TC, SC = O(N)
+}
+
 
 int main() {
     ios_base::sync_with_stdio(false);
