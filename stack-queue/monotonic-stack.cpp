027f3769252dd31b13c6c2f286d811ab51a11c53
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-03-12 00:03:37 +0530

    next greater elements II

--- stack-queue/monotonic-stack.cpp
+++ stack-queue/monotonic-stack.cpp
@@ -37,6 +37,48 @@ vector<int> nextLargerElement(vector<int> arr) {
     return ans;
 }
 
+vector<int> nextGreaterElements(vector<int> &arr) {
+    int n = arr.size();
+    vector<int> ans(n,-1);
+
+    /*
+    // Brute - O(N^2) - TC, SC - O(N)
+    for(int i = 0; i<n; i++) {
+        int ele = arr[i];
+        for(int j = i+1; j<i+n; j++) {
+            int ind = j % n;
+
+            if(arr[ind] > ele) {
+                ans[i] = arr[ind];
+                break;
+            }
+        }
+    }
+    return ans;
+    */
+    // Optimal - O(N)
+    stack<int> st;
+
+    for(int i = 2*n - 1; i>=0; i--) {
+        int ind = i % n;
+        int ele = arr[ind];
+
+        while(!st.empty() && st.top() <= ele) {
+            st.pop();
+        }
+
+        if(i < n) {
+            if(st.empty()) {
+                ans[i] = -1;
+            } else {
+                ans[i] = st.top();
+            }
+        }
+
+        st.push(ele);
+    }
+    return ans;
+}
 
 
 int main() {
