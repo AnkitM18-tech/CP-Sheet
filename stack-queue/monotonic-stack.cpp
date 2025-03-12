f17bafc1de7459c6151a7ec177ff205a9940a2a6
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-03-13 03:03:27 +0530

    sum of subarray minimums

--- stack-queue/monotonic-stack.cpp
+++ stack-queue/monotonic-stack.cpp
@@ -105,6 +105,74 @@ vector<int> asteroidCollision(vector<int> &asteroids){
     // TC , SC = O(N)
 }
 
+vector<int> findNSE(vector<int> &arr) {
+    int n = arr.size();
+    vector<int> ans(n);
+    stack<int> st;
+
+    for(int i = n-1; i>=0; i--) {
+        int ele = arr[i];
+
+        while(!st.empty() && arr[st.top()] >= arr[i]) {
+            st.pop();
+        }
+
+        ans[i] = !st.empty() ? st.top() : n;
+
+        st.push(i);
+    }
+
+    return ans;
+}
+
+vector<int> findPSEE(vector<int>& arr) {
+    int n = arr.size();
+
+    vector<int> ans(n);
+
+    stack<int> st;
+
+    for(int i = 0; i<n; i++) {
+        while(!st.empty() && arr[st.top()] > arr[i]) {
+            st.pop();
+        }
+
+        ans[i] = !st.empty() ? st.top() : -1;
+
+        st.push(i);
+    }
+    return ans;
+}
+int sumSubarrayMins(vector<int> &arr) {
+    int n = arr.size();
+    int sum = 0, mod = (1e9 + 7);
+    /*
+    // Brute - O(N^2)
+    for(int i = 0; i<n; i++) {
+        int mini = arr[i];
+        for(int j = i; j<n; j++) {
+            mini = min(arr[j],mini);
+            sum = (sum + mini) % mod;
+        }
+    }
+    */
+    // Optimal - SC, TC - O(N)
+    vector<int> nse = findNSE(arr);
+    vector<int> psee = findPSEE(arr);
+
+    for(int i = 0; i<n; i++) {
+        int left = i - psee[i];
+        int right = nse[i] - i;
+
+        long long freq = left * right * 1LL;
+
+        int val = (freq * arr[i] * 1LL) % mod;
+
+        sum = (sum + val) % mod;
+    }
+    return sum;
+}
+
 
 int main() {
     ios_base::sync_with_stdio(false);
