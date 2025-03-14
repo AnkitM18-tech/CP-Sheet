620eb64ec3256ecb00ac13adca2d70d075527b33
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-03-14 18:51:16 +0530

    LL traversal

---
+++ linked-list/starter.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    public:
+        int val;
+        ListNode* next;
+
+        ListNode() {
+            val = 0;
+            next = nullptr;
+        }
+
+        ListNode(int val1) {
+            val = val1;
+            next = nullptr;
+        }
+
+        ListNode(int val1, ListNode* next1) {
+            val = val1;
+            next = next1;
+        }
+};
+
+vector<int> LLTraversal(ListNode *head) {
+    vector<int> ans;
+    ListNode* current = head;
+    while(current != nullptr) {
+        ans.push_back(current->val);
+        current = current->next;
+    }
+    return ans; // O(N) - SC & TC
+}
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--){
+        int N;
+        cin >> N;
+        
+    }
+    return 0;
+}
