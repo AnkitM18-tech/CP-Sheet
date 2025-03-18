55df001e6cf62e9ec20616d08cbce4332479b942
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-03-18 19:21:52 +0530

    LL palindrome check

--- linked-list/faq-medium.cpp
+++ linked-list/faq-medium.cpp
@@ -88,6 +88,74 @@ ListNode *addOne(ListNode *head) {
     return head; // O(N) - TC and SC
 }
 
+ListNode *reverseLL(ListNode* head) {
+    if(head == NULL || head->next == NULL) {
+        return head;
+    }
+
+    ListNode* newHead = reverseLL(head->next);
+    ListNode* front = head->next;
+    front->next = head;
+    head->next = NULL;
+
+    return newHead;
+}
+
+bool isPalindrome(ListNode* head) {
+    /*
+    // Brute - O(2N) - TC & O(N) - SC
+    stack<int> st;
+    ListNode* temp = head;
+
+    while(temp != NULL) {
+        st.push(temp->val);
+        temp = temp->next;
+    }
+
+    temp = head;
+
+    while(temp != NULL) {
+        if(temp->val != st.top()) {
+            return false;
+        }
+        st.pop();
+
+        temp = temp->next;
+    }
+
+    return true;
+    */
+    // Optimal
+    if(head == NULL || head->next == NULL) {
+        return true;
+    }
+
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    while(fast->next != NULL && fast->next->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    ListNode* newHead = reverseLL(slow->next);
+    ListNode* first = head;
+    ListNode* second = newHead;
+
+    while(second != NULL) {
+        if(first->val != second->val) {
+            reverseLL(newHead);
+            return false;
+        }
+        first = first->next;
+        second = second->next;
+    }
+    reverseLL(newHead);
+    return true;
+    // O(N/2 + N/2 + N/2 + N/2), 
+    // which simplifies to O(2N), ultimately reducing to O(N)   
+}
+
 
 int main() {
     ios_base::sync_with_stdio(false);
