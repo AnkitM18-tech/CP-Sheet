4ac5cbabbc13e407c1fe49085b70ad56fe06f6a7
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-03-21 22:46:56 +0530

    Stack implementation using LL

--- stack-queue/fundamentals.cpp
+++ stack-queue/fundamentals.cpp
@@ -276,6 +276,57 @@ bool isValid(string str) {
     // TC, SC = O(N)
 }
 
+struct Node {
+    int val;
+    Node* next;
+    Node(int d) {
+        val = d;
+        next = NULL;
+    }
+};
+
+class LinkedListStack {
+// O(1) for push, pop, size, isEmpty, peek operation TC
+// SC = O(N)
+private:
+    Node* head;
+    int size;
+public:
+    LinkedListStack() {
+        head = NULL;
+        size = 0;
+    }
+    
+    void push(int x) {
+        Node* element = new Node(x);
+        element->next = head;
+        head = element;
+        size++;
+    }
+    
+    int pop() {
+        if(head == NULL) return -1;
+
+        int value = head->val;
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        size--;
+
+        return value;
+    }
+    
+    int top() {
+        if(head == NULL) return -1;
+
+        return head->val;
+    }
+    
+    bool isEmpty() {
+        return size == 0;
+    }
+};
+
 
 int main() {
     ios_base::sync_with_stdio(false);
