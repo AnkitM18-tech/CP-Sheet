d7e35e3313ea2080f107435f650001d3fc60470c
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-03-26 23:23:40 +0530

    longest non repeating string

--- sliding-window/constant-window.cpp
+++ sliding-window/constant-window.cpp
@@ -21,6 +21,53 @@ int maxScore(vector<int>& cardScore , int k){
     return maxSum; // O(2*k)
 }
 
+int longestNonRepeatingSubstring(string& s){
+    /*
+    // Brute - TC = O(N^2), SC = O(256)
+    int n = s.size();
+
+    int maxLen = 0;
+
+    for(int i = 0; i<n; i++) {
+
+        vector<int>hash(256,0);
+
+        for(int j = i; j<n; j++) {
+        if(hash[s[j]] == 1) break;
+
+        hash[s[j]] = 1;
+
+        int len = j - i + 1;
+        maxLen = max(maxLen,len);
+        }
+    }
+    return maxLen;
+    */
+    // Optimal
+    int n = s.size();
+    int HASHLENGTH = 256;
+    int hash[HASHLENGTH];
+
+    for(int i = 0; i<HASHLENGTH; i++) {
+    hash[i] = -1;
+    }
+
+    int l = 0, r = 0, maxLen = 0;
+
+    while(r<n) {
+    if(hash[s[r]] != -1) {
+        if(hash[s[r]] >= l) l = max(hash[s[r]] + 1, l);
+    }
+
+    int len = r - l + 1;
+    maxLen = max(maxLen,len);
+
+    hash[s[r]] = r;
+    r++;
+    }
+
+    return maxLen; // O(N)
+}
 
 
 int main() {
