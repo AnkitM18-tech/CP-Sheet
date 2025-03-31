ad068918e60f7193de86febdfd7c0a5e47668550
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-03-31 17:52:53 +0530

    number of subarrays with odd numbers

--- sliding-window/constant-window.cpp
+++ sliding-window/constant-window.cpp
@@ -449,6 +449,27 @@ int numSubarraysWithSum(vector<int>& nums, int goal) {
     return subarraysWithSumLessEqualToK(nums,goal) - subarraysWithSumLessEqualToK(nums,goal-1);
 }
 
+int helper(vector<int>& nums, int k) {
+    if(k < 0) return 0;
+    int l = 0, r = 0, count = 0, sum = 0;
+    while(r < nums.size()) {
+        sum += nums[r] % 2;
+
+        while(sum > k) {
+            sum -= nums[l] % 2;
+            l++;
+        }
+
+        count += (r-l+1);
+        r++;
+    }
+    return count;
+}
+
+int numberOfOddSubarrays(vector<int>& nums, int k) {
+    // O(2*2N)
+    return helper(nums,k)-helper(nums,k-1);
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
