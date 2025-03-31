97d7da5f20c46247321a31b62812c28cf1af0a35
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-03-31 21:22:08 +0530

    Min stack optimal

--- stack-queue/faq.cpp
+++ stack-queue/faq.cpp
@@ -32,6 +32,54 @@ public:
     // TC - O(1) and SC - O(2N)
 };
 
+class MinStackOptimal {
+private:
+    stack<int> st;
+    int mini = INT_MAX;
+public:
+    MinStackOptimal() {
+    }
+
+    void push(int value) {
+        if(st.empty()) {
+            mini = value;
+            st.push(value);
+            return;
+        }
+        if(value > mini) {
+            st.push(value);
+        } else {
+            st.push(2 * value - mini);
+            mini = value;
+        }
+    }
+
+    void pop() {
+        if(st.empty()) return;
+
+        int x = st.top();
+        st.pop();
+        if(x < mini) {
+            mini = 2 * mini - x;
+        }
+    }
+
+    int top() {
+        if(st.empty()) return -1;
+
+        int x = st.top();
+
+        if(mini < x) return x;
+
+        return mini;
+    }
+
+    int getMin() {
+        return mini;
+    }
+    // O(1) - TC and SC = O(N)
+};
+
 
 int main() {
     ios_base::sync_with_stdio(false);
