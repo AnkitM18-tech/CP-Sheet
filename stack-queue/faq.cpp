934e9e1ff2dee587066ea08fd0461f1ee472d138
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-04-01 23:51:27 +0530

    trap rainwater

--- stack-queue/faq.cpp
+++ stack-queue/faq.cpp
@@ -119,6 +119,71 @@ vector<int> maxSlidingWindow(vector<int> &arr, int k) {
     return ans;
 }
 
+vector<int> findPrefixMax(vector<int> &height, int n) {
+    vector<int> prefixMax(n);
+
+    prefixMax[0] = height[0];
+
+    for(int i = 1; i < n; i++) {
+        prefixMax[i] = max(prefixMax[i-1], height[i]);
+    }
+
+    return prefixMax;
+}
+
+vector<int> findSuffixMax(vector<int> &height, int n) {
+    vector<int> suffixMax(n);
+
+    suffixMax[n-1] = height[n-1];
+
+    for(int i = n-2; i >= 0; i--) {
+        suffixMax[i] = max(suffixMax[i+1],height[i]);
+    }
+
+    return suffixMax;
+}
+
+int trap(vector<int> &height){
+    /*
+    // Brute - O(3N) = TC and SC = O(2N)
+    int n = height.size();
+    int total = 0;
+
+    vector<int> leftMax = findPrefixMax(height,n);
+    vector<int> rightMax = findSuffixMax(height,n);
+
+    for(int i = 0; i < n; i++) {
+        if(height[i] < leftMax[i] && height[i] < rightMax[i]) {
+            total += min(leftMax[i], rightMax[i]) - height[i];
+        }
+    }
+    return total;
+    */
+    // Optimal - O(N) and SC = O(1)
+    int n = height.size();
+    int leftMax = 0, rightMax = 0, total = 0;
+    int left = 0, right = n-1;
+
+    while(left < right) {
+        if(height[left] <= height[right]) {
+            if(leftMax > height[left]) {
+                total += leftMax - height[left];
+            } else {
+                leftMax = height[left];
+            }
+            left++;
+        } else {
+            if(rightMax > height[right]) {
+                total += rightMax - height[right];
+            } else {
+                rightMax = height[right];
+            }
+            right--;
+        }
+    }
+    return total;
+}
+
 
 int main() {
     ios_base::sync_with_stdio(false);
