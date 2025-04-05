c19264fa7ba857080f120062c72b43c62f021163
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-04-06 02:21:33 +0530

    LRU cache implementation

--- stack-queue/faq.cpp
+++ stack-queue/faq.cpp
@@ -413,6 +413,92 @@ int celebrity(vector<vector<int>> &M){
     return top;
 }
 
+class Node {
+  public:
+    int key,val;
+    Node* next;
+    Node* prev;
+
+    Node() {
+      key = val = -1;
+      next = prev = NULL;
+    }
+
+    Node(int k, int value) {
+      key = k;
+      val = value;
+      next = prev = NULL;
+    }
+};
+
+class LRUCache {
+  private:
+    map<int,Node*> mpp;
+    int cap;
+    // Dummy head and tail
+    Node* head;
+    Node* tail;
+
+    void deleteNode(Node* node) {
+      Node* prevNode = node->prev;
+      Node* nextNode = node->next;
+
+      prevNode->next = nextNode;
+      nextNode->prev = prevNode;
+    }
+
+    void insertAfterHead(Node* node) {
+      Node* nextNode = head->next;
+      head->next = node;
+      nextNode->prev = node;
+      node->prev = head;
+      node->next = nextNode;
+    }
+
+  public:
+    LRUCache(int capacity) {
+      cap = capacity;
+      mpp.clear();
+
+      head = new Node();
+      tail = new Node();
+
+      head->next = tail;
+      tail->prev = head;
+    }
+
+    int get(int key_) {
+      if(mpp.find(key_) == mpp.end()) return -1;
+
+      Node* node = mpp[key_];
+      int val = node->val;
+
+      deleteNode(node);
+      insertAfterHead(node);
+      return val;
+    }
+
+    void put(int key_, int value) {
+      if(mpp.find(key_) != mpp.end()) {
+        Node* node = mpp[key_];
+        node->val = value;
+        deleteNode(node);
+        insertAfterHead(node);
+        return;
+      }
+
+      if(mpp.size() == cap) {
+        Node* node = tail->prev;
+        mpp.erase(node->key);
+        deleteNode(node);
+      }
+
+      Node* newNode = new Node(key_,value);
+      mpp[key_] = newNode;
+      insertAfterHead(newNode);
+    }
+    // O(N) - TC, N = number of queries, SC = O(capacity)
+};
 
 
 int main() {
