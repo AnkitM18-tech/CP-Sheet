952dbaa7b3007f95d1ee2b1b67463e8524524fa0
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-04-11 03:23:21 +0530

    kth element

--- binary-search/binary-search-faq.cpp
+++ binary-search/binary-search-faq.cpp
@@ -318,6 +318,33 @@ long double minimiseMaxDistance(vector<int> &arr, int k) {
     return high;
 }
 
+int kthElement(vector<int> &a, vector<int>& b, int k) {
+    int n1 = a.size(), n2 = b.size();
+    if(n1 > n2) return kthElement(b,a,k);
+    int n = n1 + n2;
+    int left = k;
+
+    int low = max(0,k-n2), high = min(n1,k);
+    while(low <= high) {
+    int mid1 = (low + high) >> 1;
+    int mid2 = left - mid1;
+
+    int l1 = mid1 > 0 ? a[mid1-1] : INT_MIN;
+    int r1 = mid1 < n1 ? a[mid1] : INT_MAX;
+    int l2 = mid2 > 0 ? b[mid2-1] : INT_MIN;
+    int r2 = mid2 < n2 ? b[mid2] : INT_MAX;
+
+    if(l1 <= r2 && l2 <= r1) {
+        return max(l1,l2);
+    } else if(l1 > r2) {
+        high = mid1 - 1;
+    } else low = mid1 + 1;
+    }
+    return 0;
+    // O(log(min(M, N))), where M and N are the sizes of two given arrays
+}
+
+
 
 int main() {
     ios_base::sync_with_stdio(false);
