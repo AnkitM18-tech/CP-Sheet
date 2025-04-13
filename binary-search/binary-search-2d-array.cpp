01a7b493c1ffb4ff541553cfaefe4c7078cdb7e3
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-04-13 18:59:47 +0530

    search matrix - I

--- binary-search/binary-search-2d-array.cpp
+++ binary-search/binary-search-2d-array.cpp
@@ -46,6 +46,62 @@ int rowWithMax1s(vector<vector<int>> & mat) {
     return index;
 }
 
+bool binarySearch(vector<int> &mat, int target) {
+    int n = mat.size();
+    int low = 0, high = n-1;
+
+    while(low <= high) {
+        int mid = (low + high) / 2;
+        if(mat[mid] == target) return true;
+        else if(mat[mid] < target) low = mid + 1;
+        else high = mid - 1;
+    }
+    return false;
+}
+
+bool searchMatrix(vector<vector<int>> &mat, int target){
+    int row = mat.size(), col = mat[0].size();
+    if(row == 0 || col == 0) {
+        return false;
+    }
+    /*
+    // Brute - O(N * M)
+
+    for(int i = 0; i < row; i++) {
+        for(int j = 0; j < col; j++) {
+            if(mat[i][j] == target) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+    
+    // Better - O(N + log(M))
+    for(int i = 0; i<row; i++) {
+        // Check if there is a possibility that
+        // the target can be found in current row
+        if(mat[i][0] <= target && target <= mat[i][col-1]) {
+            return binarySearch(mat[i], target);
+        }
+    }
+    return false;
+    */
+    // Optimal - O(log(N*M))
+    int low = 0, high = row * col - 1;
+
+    while(low <= high) {
+        int mid = (low + high) / 2;
+        // row and col calculation
+        int m = mid / col;
+        int n = mid % col;
+
+        if(mat[m][n] == target) return true;
+        else if(mat[m][n] < target) low = mid + 1;
+        else high = mid - 1;
+    }
+    return false;
+}
 
 
 int main() {
