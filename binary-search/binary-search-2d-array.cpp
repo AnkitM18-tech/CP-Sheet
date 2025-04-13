888685a897a1606bc6af5e2862bf24e436639890
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-04-14 02:06:02 +0530

    find peak element

--- binary-search/binary-search-2d-array.cpp
+++ binary-search/binary-search-2d-array.cpp
@@ -125,6 +125,45 @@ bool searchMatrix(vector<vector<int>> &matrix, int target){
     return false;
 }
 
+int maxElement(vector<vector<int>> &arr, int col) {
+    int n = arr.size();
+    int maxi = INT_MIN;
+    int index = -1;
+
+    for(int i = 0; i < n; i++) {
+        if(arr[i][col] > maxi) {
+            maxi = arr[i][col];
+            index = i;
+        }
+    }
+    return index;
+}
+
+vector<int> findPeakGrid(vector<vector<int>>& mat) {
+    int n = mat.size();
+    int m = mat[0].size();
+
+    int low = 0, high = m - 1;
+
+    while(low <= high) {
+        int mid = (low + high) / 2;
+        int row = maxElement(mat,mid);
+
+        int left = mid - 1 >= 0 ? mat[row][mid-1] : INT_MIN;
+        int right = mid + 1 < m ? mat[row][mid+1] : INT_MIN;
+
+        if(mat[row][mid] > left && mat[row][mid] > right) {
+            return {row,mid};
+        } else if(left > mat[row][mid]) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return {-1,-1};
+    // O(N * log M)
+}
 
 
 int main() {
