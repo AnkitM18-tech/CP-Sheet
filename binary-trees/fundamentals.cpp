7a98fe723c78f63330fe510463178468c8ca0435
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-04-15 16:05:45 +0530

    representation of Binary Tree

--- binary-trees/fundamentals.cpp
+++ binary-trees/fundamentals.cpp
@@ -1,19 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
 
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
 
 
 int main() {
     ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-    int t;
-    cin >> t;
-    while(t--){
-        int N;
-        cin >> N;
-        
-    }
+    
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+
+    root->left->right = new Node(5);
+
     return 0;
 }
 
