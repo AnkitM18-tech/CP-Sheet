727c497770191e1e9d6063095b83a62906fa1f6f
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-04-19 02:37:54 +0530

    balanced binary tree

--- binary-trees/medium.cpp
+++ binary-trees/medium.cpp
@@ -63,26 +63,43 @@ int getHeight(TreeNode *root) {
 }
 
 bool isBalanced(TreeNode *root){
-    // for every node of tree - heights of left and right
-    // subtrees is <= 1
-    //Brute - TC = O(N^2) SC = O(N)
-    //  For each node in the Binary Tree, 
-    // all other nodes are traversed to calculate its height, 
-    // resulting in a nested traversal structure, 
-    // leading to O(N) operations for each of the N nodes, 
-    // hence O(N * N) = O(N^2)
-    if(root == NULL) return true;
+    /*
+        // for every node of tree - heights of left and right
+        // subtrees is <= 1
+    	//Brute - TC = O(N^2) SC = O(N)
+        //  For each node in the Binary Tree, 
+        // all other nodes are traversed to calculate its height, 
+        // resulting in a nested traversal structure, 
+        // leading to O(N) operations for each of the N nodes, 
+        // hence O(N * N) = O(N^2)
+        if(root == NULL) return true;
 
-    int leftHeight = getHeight(root->left);
-    int rightHeight = getHeight(root->right);
+        int leftHeight = getHeight(root->left);
+        int rightHeight = getHeight(root->right);
 
-    if(abs(leftHeight - rightHeight) <= 1 && isBalanced(root->left) && isBalanced(root->right)) {
-        return true;
-    }
-    return false;
+        if(abs(leftHeight - rightHeight) <= 1 && isBalanced(root->left) && isBalanced(root->right)) {
+            return true;
+        }
+        return false;
+    */
+    // Optimal
+    return dfsHeight(root) != -1;
+    // TC = O(N) = SC
 }
 
+int dfsHeight(TreeNode* root) {
+    if(root == NULL) return 0;
 
+    int leftHeight = dfsHeight(root->left);
+    if(leftHeight == -1) return -1;
+    
+    int rightHeight = dfsHeight(root->right);
+    if (rightHeight == -1) return -1;
+
+    if(abs(leftHeight - rightHeight) > 1) return -1;
+
+    return max(leftHeight,rightHeight) + 1;
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
