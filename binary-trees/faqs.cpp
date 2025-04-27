00d3d88d3380e68e1252d07e1ee7e43da7fcd067
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-04-27 18:29:43 +0530

    bottom view of binary tree

--- binary-trees/faqs.cpp
+++ binary-trees/faqs.cpp
@@ -198,7 +198,33 @@ vector<int> topView(TreeNode *root){
     // TC = O(N*log N), SC = O(N)
 }
 
+vector <int> bottomView(TreeNode *root){
+    vector<int> ans;
+    if(root == NULL) return ans;
 
+    map<int,int> mpp;
+    queue<pair<TreeNode*,int>> q;
+    q.push({root,0});
+
+    while(!q.empty()) {
+    auto it = q.front();
+    q.pop();
+    TreeNode* node = it.first;
+    int line = it.second;
+    mpp[line] = node->data;
+
+    if(node->left) q.push({node->left, line-1});
+    if(node->right) q.push({node->right, line+1});
+    }
+
+    for(auto it : mpp) {
+    ans.push_back(it.second);
+    }
+    return ans;
+    // TC = O(N), SC = O(N)
+    }
+
+    
 int main() {
     ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
