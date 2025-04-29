5d462a18fbfd53b92d7f39ce0b12fb0ae9492498
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-04-29 22:57:09 +0530

    width of binary tree

--- binary-trees/faqs.cpp
+++ binary-trees/faqs.cpp
@@ -366,6 +366,49 @@ TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
     // TC = O(N) and SC = O(N)
 }
 
+int widthOfBinaryTree(TreeNode* root) {
+    if(!root) return 0;
+
+    int ans = 0;
+
+    queue<pair<TreeNode*,int>> q;
+    q.push({root, 0});
+
+    while(!q.empty()) {
+        int size = q.size();
+        int mini = q.front().second;
+
+        // Store the first and last positions 
+        // of nodes in the current level
+        int first = 0, last = 0;
+
+        for(int i = 0; i<size; i++) {
+            // Calculate current position relative
+            // to the minimum position in the level
+            int cur_id = q.front().second - mini;
+            TreeNode* node = q.front().first;
+            q.pop();
+
+            // If this is the first node in the level, 
+            // update the 'first' variable
+            if(i == 0) first = cur_id;
+
+            // If this is the last node in the level,
+            // update the 'last' variable
+            if (i == size - 1) {
+                last = cur_id;
+            }
+
+            if(node->left) q.push({node->left, cur_id * 2 + 1});
+            if(node->right) q.push({node->right, cur_id * 2 + 2});
+        }
+        ans = max(ans,last-first+1);
+    }
+    return ans;
+    // TC = O(N) and SC = O(N)
+}
+
+
 
 int main() {
     ios_base::sync_with_stdio(false);
