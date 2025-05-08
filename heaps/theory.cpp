2874980b06118842ea996a0167205879a242d67f
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-05-08 23:50:56 +0530

    heap rproperty theory

--- heaps/theory.cpp
+++ heaps/theory.cpp
@@ -1,6 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/* 
+The heap property defines the relationship between a parent node and its child nodes in a heap. This property ensures that the structure of the heap remains consistent for efficient operations. The exact nature of this relationship depends on the type of heap. There are two types of heaps:
+Min-Heap: The value of each parent node must be less than or equal to the values of its child nodes. This ensures that the smallest element is always at the root of the heap.
+Max-Heap: The value of each parent node must be greater than or equal to the values of its child nodes. This ensures that the largest element is always at the root of the heap.
+*/
 
 
 int main() {
