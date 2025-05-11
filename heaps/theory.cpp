4fb1e0c7f31f98b20a9e636859cf40d040811cc6
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-05-12 00:09:35 +0530

    Implementing max heap

--- heaps/theory.cpp
+++ heaps/theory.cpp
@@ -211,6 +211,104 @@ class MinHeap{
         int heapSize() { return count; }
 };
 
+class MaxHeap{
+    private:
+        vector<int> arr;
+        int count;
+
+        void heapifyUp(int ind) {
+            int parentInd = (ind - 1)/2;
+            if(ind > 0 && arr[ind] > arr[parentInd]) {
+                swap(arr[ind], arr[parentInd]);
+                heapifyUp(parentInd);
+            }
+            return;
+        }
+
+        void heapifyDown(int ind) {
+            int n = arr.size();
+            int largestInd = ind;
+
+            int leftChildInd = 2 * ind + 1, rightChildInd = 2 * ind + 2;
+
+            if(leftChildInd < n && arr[leftChildInd] > arr[largestInd]) {
+                largestInd = leftChildInd;
+            }
+
+            if(rightChildInd < n && arr[rightChildInd] > arr[largestInd]) {
+                largestInd = rightChildInd;
+            }
+
+            if(largestInd != ind) {
+                swap(arr[ind], arr[largestInd]);
+                heapifyDown(largestInd);
+            }
+            return;
+        }
+
+    public:
+
+        void initializeHeap(){
+            arr.clear();
+            count = 0;
+        }
+
+        void insert(int key){
+            arr.push_back(key);
+            heapifyUp(count);
+            count++;
+            return;
+        }
+
+        void changeKey(int index, int new_val){
+            if(arr[index] < new_val) {
+                arr[index] = new_val;
+                heapifyUp(index);
+            } else {
+                arr[index] = new_val;
+                heapifyDown(index);
+            }
+            return;
+        }
+
+        void extractMax(){
+            int ele = arr[0];
+
+            swap(arr[0], arr[count-1]);
+            arr.pop_back();
+            count--;
+
+            heapifyDown(0);
+        }
+
+        bool isEmpty(){
+            return count == 0;
+        }
+
+        int getMax(){
+            return arr[0];
+        }
+
+        int heapSize(){
+            return count;
+        }
+
+        /*
+        Time Complexity:
+
+        Insert(val): Inserting and Heapifying upwards contribute to O(logN) time.
+        Get Maximum(): Constant time operation, i.e., O(1).
+        Extract Maximum(): Involves Heapifying downwards contributing to O(logN) time.
+        Heap Size(): Constant time operation, i.e., O(1).
+        Is Empty(): Constant time operation, i.e., O(1).
+        Change Key(ind, val): Involves heapifying which takes O(logN) time.
+
+        Space Complexity: 
+        
+        O(N), because of the array used to store the elements.
+        */
+};
+
 
 
 int main() {
