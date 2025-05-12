9364ff619a5b1a6a9b91f296eebaba228bf5a6ec
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-05-12 20:16:07 +0530

    heap sort

--- heaps/theory.cpp
+++ heaps/theory.cpp
@@ -358,6 +358,55 @@ The recursive calls during heapify require stack space proportional to the heigh
 the heap which will be of the order of log(N) in the worst-case.
 */
 
+void heapifyDown(vector<int>& arr,int last, int ind) {
+    int n = arr.size();
+    int largestInd = ind;
+    int leftChildInd = 2 * ind + 1, rightChildInd = 2 * ind + 2;
+
+    if(leftChildInd <= last && arr[leftChildInd] > arr[largestInd]) largestInd = leftChildInd;
+    if(rightChildInd <= last && arr[rightChildInd] > arr[largestInd]) largestInd = rightChildInd;
+
+    if(largestInd != ind) {
+        swap(arr[largestInd], arr[ind]);
+        heapifyDown(arr,last,largestInd);
+    }
+    return;
+}
+
+void buildMaxHeap(vector<int>& nums) {
+    int n = nums.size();
+
+    for(int i = n/2 - 1; i >= 0; i--) {
+        heapifyDown(nums,n-1,i);
+    }
+    return;
+}
+
+void heapSort(vector<int>&nums) {
+    buildMaxHeap(nums);
+    int last = nums.size() - 1;
+
+    while(last > 0) {
+        swap(nums[0], nums[last]);
+        last--;
+
+        if(last > 0) {
+            heapifyDown(nums,last,0);
+        }
+    }
+    return;
+    /*
+    Time Complexity: O(N*logN), where N is the size of the array
+    Building a max-heap from the array takes O(N) iterations. Once done, 
+    each node is placed at its correct index and the array is heapified 
+    (which takes logN iterations) taking overall O(N*logN) time.
+
+    Space Complexity: O(logN)
+    Recursive stack space used while building the max-heap is O(logN). 
+    Also, the depth of each heapify Down will take O(logN) space.
+    */
+}
+
 
 
 int main() {
