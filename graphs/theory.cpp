f9c7810e773a5eacc236862045a09c8b6078e0b2
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-05-24 18:30:26 +0530

    Graphs Theory

--- graphs/theory.cpp
+++ graphs/theory.cpp
@@ -69,15 +69,96 @@ Note: In applications, weight may be a measure of the cost of a route. For examp
 
 */
 
+/* 
+
+In the question, it will be mentioned whether the graph is directed or undirected.
+
+The first line contains two space-separated integers n and m denoting the number of nodes and the number of edges respectively.
+Next m lines contain two integers u and v representing an edge between u and v. In the case of an undirected graph, if there is an edge between u and v, it means there is an edge between v and u as well.
+Note: There is no upper bound on the value of m.
+
+The first hurdle while solving any graph problem is how to represent the graphs in the programming languages. The two most commonly used representations for graphs are:
+
+Adjacency Matrix
+Adjacency Lists
+
+Adjacency Matrix:
+An adjacency matrix of a graph is a two-dimensional array of size n x n, where n is the number of nodes in the graph, with the property that a[i][j] = 1 if the edge (vᵢ, vⱼ) is in the set of edges, and a[i][j] = 0 if there is no such edge.
+Consider the example of the following input:
+Input:
+    5 6
+    1 2
+    1 3
+    2 4
+    3 4 
+    3 5 
+    4 5
+
+Explanation:
+    Number of nodes, n = 5
+    Number of edges, m = 6
+Next m lines represent the edges.
+
+Adjacency List:
+This is a node-based representation. In this representation, each node is associated with a list of nodes adjacent to it. Normally an array is used to store the nodes. The array provides random access to the adjacency list for any particular node.
+
+To create an adjacency list, an array of size n+1 must be created, where n is the number of nodes. 
+
+Weighted Graph Representation:
+
+For adjacency matrix: The weight of the edge between node u and node v can be stored at the location a[u][v].
+
+For adjacency list: Earlier in the adjacency list, a list of integers was stored in each index, but for weighted graphs, a list of pairs (adjacent node, edge weight) will be stored for each node - vector<pair<int,int>> adjList[n+1]
+
+*/
+
 int main() {
     ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
     int t;
     cin >> t;
     while(t--){
-        int N;
-        cin >> N;
+        // Adjacency Matrix Way
+        // Taking the input 
+        int n, m;
+        cin >> n >> m;
         
+        // Adjacency matrix to store the graph - 1 based indexing
+        // int adj[n+1][n+1];
+
+        // adjacency list for undirected graph
+        vector<int> adj[n+1];
+        
+        // Add all the edges in the graph 
+        for(int i = 0; i < m; i++) {
+            // Taking the input
+            int u, v;
+            cin >> u >> v;
+            
+            // Adding the edges - matrix
+            // adj[u][v] = 1;
+            
+            /* This statement will be removed 
+            in the case of directed graph */
+            // adj[v][u] = 1;
+
+            // Adding the edges - List
+            adj[u].push_back(v);
+            /* This statement will be removed 
+            in the case of directed graph */
+            adj[v].push_back(u);
+        }
+        /*  
+            Adjacency Matrix -
+            Space Complexity: O(N^2) The space needed to represent a graph using its adjacency matrix is N² locations. It is a costly method as n² locations are consumed. It is preferred for dense graphs where the number of edges is more.
+
+            Note: For directed graphs, if there is an edge between u and v it means the edge only goes from u to v, i.e., v is the neighbor of u, but vice versa is not true.
+            
+            Adjacency List -
+            Space Complexity: O(2xE) The space needed to represent a graph using its adjacency matrix is N² locations. This representation is much better than the adjacency matrix for sparse graphs (where the number of edges is less), as matrix representation consumes n² locations, and most of them are unused.
+
+            Note: For directed graphs, if there is an edge between u and v it means the edge only goes from u to v, i.e., v is the neighbor of u, but vice versa is not true. Thus, The space needed to represent a directed graph using its adjacency list is E (O(E)) locations, where E denotes the number of edges, as here each edge data appears only once
+        */
     }
     return 0;
 }
