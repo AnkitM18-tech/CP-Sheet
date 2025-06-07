59a27a950fa8e1b6119c08b91bee9d4ade290cc0
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-06-08 02:48:01 +0530

    path with minimum efforts

--- graphs/shortest-path-algorithm.cpp
+++ graphs/shortest-path-algorithm.cpp
@@ -243,6 +243,72 @@ public:
     */
 };
 
+#define P pair <int, pair<int,int>>
+
+class PathWithMinimumEffort {
+private:
+    vector<int> delRow = {-1, 0, 1, 0};
+    vector<int> delCol = {0, 1, 0, -1};
+
+    bool isValid(int& row, int& col, int& n, int& m) {
+        if(row < 0 || row >= n) return false;
+        if(col < 0 || col >= m) return false;
+        return true;
+    }
+public:
+    int MinimumEffort(vector<vector<int>> &heights) {
+        int n = heights.size();
+        int m = heights[0].size();
+
+        vector<vector<int>> maxDiff(n, vector<int>(m, 1e9));
+        priority_queue<P, vector<P>, greater<P>> pq;
+
+        // Mark the initial position as 0
+        maxDiff[0][0] = 0;
+
+        // starting point
+        pq.push({0, {0,0}});
+
+        while(!pq.empty()) {
+            auto p = pq.top();
+            int diff = p.first;
+
+            int row = p.second.first;
+            int col = p.second.second;
+            pq.pop();
+
+            if(row == n - 1 && col == m - 1) return diff;
+
+            for(int i = 0; i < 4; i++) {
+                int newRow = row + delRow[i];
+                int newCol = col + delCol[i];
+
+                if(isValid(newRow, newCol, n, m)) {
+                    int currDiff = abs(heights[newRow][newCol] - heights[row][col]);
+                    if(max(currDiff, diff) < maxDiff[newRow][newCol]) {
+                        maxDiff[newRow][newCol] = max(currDiff, diff);
+                        pq.push({max(currDiff, diff), {newRow, newCol}});
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+    /*
+        Time Complexity: O(N*M*log(N*M))
+
+        The algorithm processes each cell and explores its neighbors using a 
+        priority queue taking O(4*N*M) time.
+        The priority queue operations (insertion and extraction) are logarithmic 
+        in nature making overall complexity as O(4*N*M*log(N*M)).
+        Space Complexity: O(N*M)
+
+        Matrix to store maximum differences for each cell takes O(N*M) space.
+        Priority queue will store N*M elements in worst case contributing to O(N*M) 
+        space.
+    */
+};
+
 
 
 int main() {
