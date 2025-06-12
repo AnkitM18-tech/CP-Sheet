e1ff15dbbebc9815e49c28cdecd7b324b1450365
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-06-12 20:04:12 +0530

    Krushkal's Algorithm

--- graphs/minimum-spanning-tree.cpp
+++ graphs/minimum-spanning-tree.cpp
@@ -125,6 +125,109 @@ class PrimsAlgorithm{
     */
 };
 
+#define P pair<int,int>
+
+class DisjointSet {
+    vector<int> rank, parent, size;
+
+    public:
+        DisjointSet(int n) {
+            rank.resize(n + 1, 0);
+            parent.resize(n + 1);
+            size.resize(n + 1, 1);
+
+            for(int i = 0 ; i <= n; i++) {
+                parent[i] = i;
+            }
+        }
+
+        int findUPar(int node) {
+            if(node == parent[node]) return node;
+            return parent[node] = findUPar(parent[node]);
+        }
+
+        void unionByRank(int u, int v) {
+            int ulp_u = findUPar(u);
+            int ulp_v = findUPar(v);
+
+            if(ulp_u == ulp_v) return;
+
+            if(rank[ulp_u] < rank[ulp_v]) {
+                parent[ulp_u] = ulp_v;
+            } else if(rank[ulp_v] < rank[ulp_u]) {
+                parent[ulp_v] = ulp_u;
+            } else {
+                parent[ulp_v] = ulp_u;
+                rank[ulp_u]++;
+            }
+        }
+
+        void unionBySize(int u, int v) {
+            int ulp_u = findUPar(u);
+            int ulp_v = findUPar(v);
+
+            if(ulp_u == ulp_v) return;
+
+            if(size[ulp_u] < size[ulp_v]) {
+                parent[ulp_u] = ulp_v;
+                size[ulp_v] += size[ulp_u];
+            } else {
+                parent[ulp_v] = ulp_u;
+                size[ulp_u] += size[ulp_v];
+            }
+        }
+};
+
+class KrushkalAlgorithm {
+    public:
+    int spanningTree(int V, vector<vector<int>> adj[]) {
+        vector<pair<int, pair<int,int>>> edges;
+
+        for(int i = 0; i < V; i++) {
+            for(auto it : adj[i]) {
+                int v = it[0];
+                int wt = it[1];
+                int u = i;
+                edges.push_back({wt, {u, v}});
+            }
+        }
+
+        DisjointSet ds(V);
+
+        // Sorting the edges based on their weights
+        sort(edges.begin(), edges.end());
+
+        int sum = 0;
+
+        for(auto it : edges) {
+            int wt = it.first;
+            int u = it.second.first;
+            int v = it.second.second;
+
+            if(ds.findUPar(u) != ds.findUPar(v)) {
+                sum += wt;
+                ds.unionBySize(u, v);
+            }
+        }
+
+        return sum;
+    }
+
+    /*
+        Time Complexity: O(V+E) + O(ElogE) + O(E*4α*2) (where V and E are the numbers 
+        of nodes and edges in the graph)
+
+        Extracting edges from adjacency list takes O(V+E) time.
+        Sorting the edges takes O(ElogE) time.
+        In the worst case, all the edges are iterated and added to the disjoint set. 
+        The find parent and union operation takes 4α time making overall time complexity 
+        as O(E*4α*2).
+        Space Complexity: O(V+E)
+
+        Storing the edges take O(E) space.
+        The disjoint set on V vertices take O(V) space due to parent, rank/size arrays.
+    */
+};
 
 
 int main() {
