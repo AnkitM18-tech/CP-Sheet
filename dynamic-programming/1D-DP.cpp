f506f55a60ca8c1c5c198613aefdc0a45c0de096
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-06-15 02:25:08 +0530

    Climbing Stairs

---
+++ dynamic-programming/1D-DP.cpp
@@ -0,0 +1,115 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class ClimbingStairs {
+    private:
+        int func(int n, vector<int>& dp) {
+            if(n <= 1) return 1;
+    
+            if(dp[n] != -1) return dp[n];
+    
+            return dp[n] = func(n-1, dp) + func(n-2, dp);
+        }
+    
+    public:
+        int climbStairs(int n) {
+            if(n == 0 || n == 1) return 1;
+            /*
+            int oneStep = climbStairs(n - 1);
+            int twoSteps = climbStairs(n - 2);
+    
+            return oneStep + twoSteps;
+            */
+            /*
+            vector<int> dp(n + 1, -1);
+            return func(n, dp);
+            */
+            /*
+            vector<int> dp(n + 1, -1);
+            dp[0] = 1;
+            dp[1] = 1;
+    
+            for(int i = 2; i <= n; i++) {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+    
+            return dp[n];
+            */
+            int prev2 = 1;
+            int prev = 1;
+    
+            for(int i = 2; i <= n; i++) {
+                int cur_i = prev + prev2;
+                prev2 = prev;
+                prev = cur_i;
+            }
+    
+            return prev;
+        }
+    
+        /*
+            Recursion - 
+            Time Complexity: O(2N), where N is the given number of stairs. 
+            The number of recursive calls roughly follows a Fibonacci-like sequence, 
+            where climbStairs(n) is approximately 2N. This is because each call branches 
+            into two more calls, leading to an exponential growth in the number of calls.
+    
+            Space Complexity:The space complexity of this recursive approach is O(n). 
+            This is because the maximum depth of the recursion stack can go up to n, 
+            due to the linear nature of the stack usage relative to the input size n.
+    
+            Memoization - 
+            Time Complexity: O(N), where N is the given number of stairs. 
+            This is because we perform a linear number of operations relative to the 
+            input size n.
+    
+            Space Complexity:O(N)+O(N), We are using a recursion stack space O(N) and an 
+            array (again O(N)). Therefore total space complexity will be O(N) + O(N) ≈ O(N).
+    
+            Tabulation - 
+            Time Complexity: O(N), where N is the given number of stairs. 
+            This is because we perform a linear number of operations relative to the 
+            input size N.
+    
+            Space Complexity:O(N), an additional array dp of size n + 1 is used to store 
+            intermediate results. Hence, the space complexity is O(N).
+    
+            Space Optimization - 
+            Time Complexity: O(N), where N is the given number of stairs. 
+            This is because we perform a linear number of operations relative to the 
+            input size n.
+    
+            Space Complexity:O(1). As no extra space is being used here.
+        */
+};
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--){
+        int N;
+        cin >> N;
+        
+    }
+    return 0;
+}
+
+/* 
+
+How to identify a problem that can be solved using 1D DP?
+
+Generic Problem Ask => 
+    - Count the total number of ways
+    - Find the Max / Min out of the total number of ways
+    - Try all Possible ways - whether count / best possible way - that's when we tend to use recursion. 
+
+Way to solve the problems related to DP - 
+    - try to represent the problem in terms of index
+    - do all possible stuff on that index acc. to the problem statement
+    - If the question states "count all the ways," then the sum of all choices/ways should be returned. If the question asks to find "the maximum/minimum", then the choice/way with the maximum/minimum output should be returned.
+
+*/
