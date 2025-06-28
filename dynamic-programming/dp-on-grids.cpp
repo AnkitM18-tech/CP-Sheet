93b5272cbc57d6702a73da1e8508617e2f8197d3
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-06-28 20:23:33 +0530

    Minimum Triangle Sum

--- dynamic-programming/dp-on-grids.cpp
+++ dynamic-programming/dp-on-grids.cpp
@@ -306,6 +306,95 @@ class MinimumFallingPathSum {
         */
 };
 
+class MinTriangleSum {
+    private:
+        int func(int i, int j, int n, vector<vector<int>>& triangle, vector<vector<int>>& dp) {
+            
+            if(dp[i][j] != -1) return dp[i][j];
+            if(i == n - 1) return triangle[i][j];
+    
+            int down = triangle[i][j] + func(i + 1, j, n, triangle, dp);
+            int diagonal = triangle[i][j] + func(i + 1, j + 1, n, triangle, dp);
+    
+            return dp[i][j] = min(down, diagonal);
+        }
+    
+        int func(vector<vector<int>>& triangle, int n, vector<vector<int>>& dp) {
+    
+            for(int j = 0; j < n; j++) {
+                dp[n-1][j] = triangle[n-1][j];
+            }
+    
+            for(int i = n - 2; i >= 0; i--) {
+                for(int j = i; j >= 0; j--) {
+                    int down = triangle[i][j] + dp[i + 1][j];
+                    int diagonal = triangle[i][j] + dp[i + 1][j + 1];
+                    
+                    dp[i][j] = min(down, diagonal);
+                }
+            }
+            return dp[0][0];
+        }
+    
+        int func(vector<vector<int>>& triangle, int n) {
+            vector<int> front(n, 0);
+            vector<int> cur(n, 0);
+    
+            for(int j = 0; j < n; j++) {
+                front[j] = triangle[n-1][j];
+            }
+    
+            for(int i = n - 2; i >= 0; i--) {
+                for(int j = i; j >= 0; j--) {
+                    int down = triangle[i][j] + front[j];
+                    int diagonal = triangle[i][j] + front[j+1];
+    
+                    cur[j] = min(down, diagonal);
+                }
+                front = cur;
+            }
+    
+            return front[0];
+        }
+    public:
+        int minTriangleSum(vector<vector<int>>& triangle) {
+            int n = triangle.size();
+            // vector<vector<int>> dp(n, vector<int>(n, -1));
+            // return func(0, 0, n, triangle, dp);
+            // return func(triangle, n, dp);
+            return func(triangle, n);
+        }
+        /*  
+            // Recursion
+            Time Complexity: O(2^N), where N is the number of rows . As, each cell has 
+            2 choices and at max the number of subproblems can be N.
+    
+            Space Complexity:O(N), The depth of the recursion is proportional to the 
+            height of the triangle N. Therefore, the space used by the call stack is O(N).
+    
+            // Memoization
+            Time Complexity: O(N*N), where N is the number of rows. As the total number 
+            of different subproblems can go upto N.
+    
+            Space Complexity:O(N) + O(N*N), We are using a recursion stack space of N 
+            and an extra DP array of size N*N.
+    
+            // Tabulation
+            Time Complexity: O(N*N), where N is the number of rows. As the whole triangle 
+            is traversed once using two nested loops.
+    
+            Space Complexity:O(N*N), As an external DP Array of size ‘N*N’ is used to 
+            store the intermediate calculations.
+    
+            // Space Optimization
+            Time Complexity: O(N*N), where N is the number of rows. As the whole triangle 
+            is traversed once using two nested loops.
+    
+            Space Complexity:O(N)+O(N), As two external array of size ‘N’ is used to store 
+            the intermediate calculations of rows.
+        */
+};
+
 
 
 int main() {
