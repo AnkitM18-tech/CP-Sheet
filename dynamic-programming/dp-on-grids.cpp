a95bb99efbd8460341dab33673cf2d5c8aab9ea8
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-06-29 21:09:08 +0530

    cherry pickup II

--- dynamic-programming/dp-on-grids.cpp
+++ dynamic-programming/dp-on-grids.cpp
@@ -395,7 +395,174 @@ class MinTriangleSum {
         */
 };
 
-
+class CherryPickupII {
+    private:
+        int func(int i, int j1, int j2, int n, int m, vector<vector<int>>& matrix, vector<vector<vector<int>>>& dp) {
+            if(j1 < 0 || j1 >= m || j2 < 0 || j2 >= m) return -1e9;
+    
+            if(i == n - 1) {
+                if(j1 == j2) return matrix[i][j1];
+                else return matrix[i][j1] + matrix[i][j2];
+            }
+    
+            if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
+    
+            int maxi = INT_MIN;
+    
+            for(int di = -1; di <= 1; di++) {
+                for(int dj = -1; dj <= 1; dj++) {
+                    int ans = 0;
+    
+                    if(j1 == j2) {
+                        ans = matrix[i][j1] + func(i + 1, j1 + di, j2 + dj, n, m, matrix, dp);
+                    } else {
+                        ans = matrix[i][j1] + matrix[i][j2] + func(i + 1, j1 + di, j2 + dj, n, m, matrix, dp);
+                    }
+    
+                    maxi = max(maxi, ans);
+                }
+            }
+    
+            return dp[i][j1][j2] = maxi;
+        }
+    
+        int func(int n, int m, vector<vector<int>>& matrix) {
+            vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, 0)));
+    
+            // Initialize the DP array for the last row
+            for(int j1 = 0; j1 < m; j1++) {
+                for(int j2 = 0; j2 < m; j2++) {
+                    if(j1 == j2) dp[n-1][j1][j2] = matrix[n-1][j1];
+                    else dp[n-1][j1][j2] = matrix[n-1][j1] + matrix[n-1][j2];
+                }
+            }
+    
+            /* Outer nested loops for traversing the DP array
+            from the second-to-last row up to the first row*/
+            for(int i = n-2; i >= 0; i--) {
+                for(int j1 = 0; j1 < m; j1++) {
+                    for(int j2 = 0; j2 < m; j2++) {
+                        int maxi = INT_MIN;
+    
+                        // Inner nested loops to try out 9 options 
+                        for(int di = -1; di <= 1; di++) {
+                            for(int dj = -1; dj <= 1; dj++) {
+                                int ans;
+    
+                                if(j1 == j2) ans = matrix[i][j1];
+                                else ans = matrix[i][j1] + matrix[i][j2];
+    
+                                if((j1 + di < 0 || j1 + di >= m) || (j2 + dj < 0 || j2 + dj >= m)) {
+                                    ans += -1e9;
+                                } else {
+                                    ans += dp[i+1][j1+di][j2+dj];
+                                }
+    
+                                maxi = max(maxi, ans);
+                            }
+                        }
+    
+                        dp[i][j1][j2] = maxi;
+                    }
+                }
+            }
+    
+            return dp[0][0][m-1];
+        }
+    
+        int func2(int n, int m, vector<vector<int>>& matrix) {
+            vector<vector<int>> front(m, vector<int>(m, 0));
+            vector<vector<int>> cur(m, vector<int>(m, 0));
+    
+            for(int j1 = 0; j1 < m; j1++) {
+                for(int j2 = 0; j2 < m; j2++) {
+                    if(j1 == j2) front[j1][j2] = matrix[n-1][j1];
+                    else front[j1][j2] = matrix[n-1][j1] + matrix[n-1][j2];
+                }
+            }
+    
+            for(int i = n-2; i >= 0; i--) {
+                for(int j1 = 0; j1 < m; j1++) {
+                    for(int j2 = 0; j2 < m; j2++) {
+                        int maxi = INT_MIN;
+    
+                        for(int di = -1; di <= 1; di++) {
+                            for(int dj = -1; dj <= 1; dj++) {
+                                int ans;
+    
+                                if(j1 == j2) {
+                                    ans = matrix[i][j1];
+                                } else {
+                                    ans = matrix[i][j1] + matrix[i][j2];
+                                }
+    
+                                if((j1 + di < 0 || j1 + di >= m) || (j2 + dj < 0 || j2 + dj >= m)) {
+                                    ans += -1e9;
+                                } else {
+                                    ans += front[j1 + di][j2 + dj];
+                                }
+    
+                                maxi = max(ans, maxi);
+                            }
+                        }
+                        
+                        cur[j1][j2] = maxi;
+                    }
+                }
+    
+                front = cur;
+            }
+    
+            return front[0][m-1];
+        }
+    
+    public:
+        int cherryPickup(vector<vector<int>>& matrix) {
+            int n = matrix.size();
+            int m = matrix[0].size();
+    
+            // vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, -1)));
+    
+            // return func(0, 0, m -1, n, m, matrix, dp);
+            // return func(n, m, matrix);
+            return func2(n, m, matrix);
+        }
+        /*
+            // Recursion
+            Time Complexity: O(3^(N)*3^(N)), where N is the number of row. 
+            As, for each cell, each of the robots has 3 options and there are N rows 
+            to be considered.
+    
+            Space Complexity:O(N), at maximum the depth of the recursive stack 
+            can go upto N.
+    
+            // Memoization
+            Time Complexity: O(N*M*M) * 9. At max, there will be N*M*M calls of recursion 
+            to solve a new problem and in every call, two nested loops together run 
+            for 9 times.
+    
+            Space Complexity:O(N) + O(N*M*M). We are using a recursion stack space of O(N), 
+            where N is the path length and an external DP Array of size ‘N*M*M’.
+    
+            // Tabulation
+            Time Complexity: O((N*M*M)*9), The outer nested loops run for (N*M*M) times 
+            and the inner two nested loops run for 9 times
+    
+            Space Complexity:O(N*M*M), As an external array of size ‘N*M*M’ is used.
+    
+            // Space Optimization
+            Time Complexity: O((N*M*M)*9), The outer nested loops run for (N*M*M) times 
+            and the inner two nested loops run for 9 times
+    
+            Space Complexity:O(N*M*M), As an external array of size ‘N*M*M’ is used.
+    
+            // Space Optimization
+            Time Complexity: O((N*M*M)*9), The outer nested loops run for (N*M*M) times 
+            and the inner two nested loops run for 9 times
+    
+            Space Complexity:O(M*M). As an external array of size ‘M*M’ is used.
+        */
+};
 
 int main() {
     ios_base::sync_with_stdio(false);
