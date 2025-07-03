4e93eca22376fc457b694e7e84fec0125e8595f3
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-07-04 02:04:23 +0530

    partition equal subset sum

--- dynamic-programming/dp-on-subsequences.cpp
+++ dynamic-programming/dp-on-subsequences.cpp
@@ -82,6 +82,102 @@ class SubsetSumEqualsTarget {
         // Space Optimization - TC = O(N*target), SC = O(target)
 };
 
+class PartitionEqualSubsetSum {
+    private:
+        bool func(int ind, int target, vector<int>& arr, vector<vector<int>>& dp) {
+            /* Base case: If the target sum is
+            0, we found a valid partition*/
+            if(target == 0) return true;
+    
+            /* Base case: If we have considered all elements 
+            and the target is still not 0, return false*/
+            if(ind == 0) return arr[0] == target;
+    
+            if(dp[ind][target] != -1) return dp[ind][target];
+    
+            bool notTaken = func(ind - 1, target, arr, dp);
+            bool taken = false;
+            if(arr[ind] <= target) {
+                taken = func(ind - 1, target - arr[ind], arr, dp);
+            }
+    
+            return dp[ind][target] = notTaken || taken;
+        }
+    
+        bool func2(int k, int n, vector<int>& arr) {
+            vector<vector<bool>> dp(n, vector<bool>(k + 1, false));
+    
+            for(int i = 0; i < n; i++) {
+                dp[i][0] = true;
+            }
+    
+            if(arr[0] <= k) {
+                dp[0][arr[0]] = true;
+            }
+    
+            for(int ind = 1; ind < n; ind++) {
+                for(int target = 1; target <= k; target++) {
+                    bool notTaken = dp[ind-1][target];
+    
+                    bool taken = false;
+                    if(arr[ind] <= target) {
+                        taken = dp[ind-1][target - arr[ind]];
+                    }
+                    
+                    dp[ind][target] = notTaken || taken;
+                }
+            }
+    
+            return dp[n-1][k];
+        }
+    
+        bool func(int n, int k, vector<int>& arr) {
+            vector<bool> prev(k + 1, false);
+    
+            prev[0] = true;
+    
+            if(arr[0] <= k) prev[arr[0]] = true;
+    
+            for(int ind = 1; ind < n; ind++) {
+                vector<bool> cur(k + 1, false);
+                cur[0] = true;
+    
+                for(int target = 1; target <= k; target++) {
+                    bool notTaken = prev[target];
+    
+                    bool taken = false;
+                    if(arr[ind] <= target) taken = prev[target - arr[ind]];
+    
+                    cur[target] = notTaken || taken;
+                }
+                prev = cur;
+            }
+            return prev[k];
+        }
+    
+    public:
+        bool equalPartition(int n, vector<int> arr) {
+            int totalSum = 0;
+    
+            for(int i = 0; i < n; i++) {
+                totalSum += arr[i];
+            }
+    
+            if(totalSum % 2 == 1) return false;
+            else {
+                int k = totalSum / 2;
+                // vector<vector<int>> dp(n, vector<int>(k+1, -1));
+                // return func(n - 1, k, arr, dp);
+                // return func2(k, n, arr);
+                return func(n, k, arr);
+            }
+        }
+        // Recursion - TC = O(2^N), SC = O(N)
+        // Memoization - TC = O(N*target, SC = O(N*target) + O(N)
+        // Tabulation - TC = O(N*target), SC = O(N*target)
+        // Space Optimization - TC = O(N*target), SC = O(target)
+};    
+
 
 
 int main() {
