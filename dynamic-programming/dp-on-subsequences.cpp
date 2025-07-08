8d75068063e89527cf2e2277fadc8f09f100495e
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-07-09 02:25:42 +0530

    0 & 1 Knapsack

--- dynamic-programming/dp-on-subsequences.cpp
+++ dynamic-programming/dp-on-subsequences.cpp
@@ -463,6 +463,77 @@ int countPartitions(int n, int diff, vector<int>& arr) {
 // Space Optimization - TC = O(N*K), SC = O(K)
 };
 
+class KnapSackProblem01{
+    private:
+    int func(vector<int>& wt, vector<int>& val, int ind, int W, vector<vector<int>>& dp) {
+        if(ind == 0) {
+            if(wt[0] <= W) return val[0];
+            else return 0;
+        } 
+    
+        if(dp[ind][W] != -1) return dp[ind][W];
+    
+        int notTaken = func(wt, val, ind - 1, W, dp);
+        int taken = 0;
+    
+        if(wt[ind] <= W) {
+            taken = val[ind] + func(wt, val, ind - 1, W - wt[ind], dp);
+        }
+    
+        return dp[ind][W] = max(notTaken, taken);
+    }
+    public:
+    int knapsack01(vector<int>& wt, vector<int>& val, int n, int W) {
+        // vector<vector<int>> dp(n, vector<int>(W + 1, -1));
+        // return func(wt, val, n - 1, W, dp);
+        // vector<vector<int>> dp(n, vector<int>(W + 1, 0));
+    
+        /* Base condition: Fill in the first 
+        row for the weight of the first item */
+        /*
+        for(int i = wt[0]; i <= W; i++) {
+            dp[0][i] = val[0];
+        }
+    
+        for(int ind = 1; ind < n; ind++) {
+            for(int cap = 0; cap <= W; cap++) {
+                int notTaken = dp[ind - 1][cap];
+                int taken = INT_MIN;
+    
+                if(wt[ind] <= cap) {
+                    taken = val[ind] + dp[ind - 1][cap - wt[ind]];
+                }
+    
+                dp[ind][cap] = max(notTaken, taken);
+            }
+        }
+    
+        return dp[n-1][W];
+        */
+        vector<int> prev(W + 1, 0);
+        for(int i = wt[0]; i <= W; i++) prev[i] = val[0];
+    
+        for(int ind = 1; ind < n; ind++) {
+            for(int cap = W; cap >= 0; cap--) {
+                int notTaken = prev[cap];
+                int taken = INT_MIN;
+    
+                if(wt[ind] <= cap) {
+                    taken = val[ind] + prev[cap - wt[ind]];
+                }
+    
+                prev[cap] = max(notTaken, taken);
+            }
+        }
+    
+        return prev[W];
+    }
+    // Recursion - TC = O(2^N), SC = O(N)
+    // Memoization - TC = O(N*W), SC = O(N*W) + O(N)
+    // Tabulation - TC = O(N*W), SC = O(N*W)
+    // Space Optimization - TC = O(N*W), SC = O(W)
+};
+
 
 
 int main() {
