67003bb934cc15a3f7d01fb3b1689195fdb13653
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-07-16 20:49:55 +0530

    minimum insertions to make string palindrome

--- dynamic-programming/dp-on-strings.cpp
+++ dynamic-programming/dp-on-strings.cpp
@@ -162,6 +162,58 @@ public:
     // Space Optimization - TC = O(N*N), SC = O(N)
 };
 
+class MinInsertionsToMakeStringPalindrome{
+private:
+    int lcs(string& s1, string& s2) {
+        int n = s1.size();
+        int m = s2.size();
+
+        /*
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+
+        for(int i = 0; i <= n; i++) dp[i][0] = 0;
+        for(int i = 0; i <= m; i++) dp[0][i] = 0;
+
+        */
+        vector<int> prev(m + 1, 0), cur(m + 1, 0);
+
+        for(int ind1 = 1; ind1 <= n; ind1++) {
+            for(int ind2 = 1; ind2 <= m; ind2++) {
+                if(s1[ind1 - 1] == s2[ind2 - 1]) {
+                    // dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+                    cur[ind2] = 1 + prev[ind2 - 1];
+                } else {
+                    // dp[ind1][ind2] = max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+                    cur[ind2] = max(prev[ind2], cur[ind2 - 1]);
+                }
+            }
+            prev = cur;
+        }
+
+        // return dp[n][m];
+        return prev[m];
+    }
+
+    int longestPalindromeSubsequence(string& s) {
+        string t = s;
+        reverse(s.begin(), s.end());
+        return lcs(s, t);
+    }
+
+public: 
+    int minInsertion(string s) {
+        int n = s.size();
+        int k = longestPalindromeSubsequence(s);
+
+        return n - k;
+    }
+
+    // Tabulation - TC = O(N*N), SC = O(N*N)
+    // Space Optimization - TC = O(N*N), SC = O(N)
+    // max operations - len(s) for the string to become palindrome
+    // min - keep the longest palindrome and add the rest
+};
+
 
 
 int main() {
