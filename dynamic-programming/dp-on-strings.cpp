5eeaf48b8999d12f554a1e861a3fad7067c2cb0e
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-07-17 00:24:49 +0530

    Minimum operations to convert string A to string B

--- dynamic-programming/dp-on-strings.cpp
+++ dynamic-programming/dp-on-strings.cpp
@@ -214,6 +214,46 @@ public:
     // min - keep the longest palindrome and add the rest
 };
 
+class MinOperationsToConvertStringAtoStringB{
+private:
+    int lcs(string& s1, string& s2) {
+        int n = s1.size(), m = s2.size();
+
+        // vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+        vector<int> prev(m + 1, 0), cur(m + 1, 0);
+
+        // for(int i = 0; i <= n; i++) dp[i][0] = 0;
+        // for(int i = 0; i <= m; i++) dp[0][i] = 0;
+
+        for(int ind1 = 1; ind1 <= n; ind1++) {
+            for(int ind2 = 1; ind2 <= m; ind2++) {
+                if(s1[ind1 - 1] == s2[ind2 - 1]) {
+                    // dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+                    cur[ind2] = 1 + prev[ind2 - 1];
+                } else {
+                    // dp[ind1][ind2] = max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+                    cur[ind2] = max(prev[ind2], cur[ind2 - 1]);
+                }
+            }
+            prev = cur;
+        }
+
+        // return dp[n][m];
+        return prev[m];
+    }
+
+public: 
+    int minOperations(string str1, string str2) { 
+        int n = str1.size(), m = str2.size();
+
+        int k = lcs(str1, str2);
+
+        return (n - k) + (m - k);
+	}
+    // max operations = n + m
+    // min operations = sum(length - lcs)
+};
+
 
 
 int main() {
