23f63555975947f4b02e0967cd69c7a3cd641ae0
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-07-17 17:53:56 +0530

    distinct subsequence

--- dynamic-programming/dp-on-strings.cpp
+++ dynamic-programming/dp-on-strings.cpp
@@ -312,6 +312,87 @@ public:
     // TC = O(N * M), SC = O(N * M)
 };
 
+class DistinctSubsequences {
+private:
+    int mod = 1e9 + 7;
+    int countUtil(string s1, string s2, int ind1, int ind2, vector<vector<int>>& dp) {
+        /* If s2 has been completely matched,
+        return 1 (found a valid subsequence)*/
+        if(ind2 == 0) return 1;
+        /* If s1 has been completely traversed
+        but s2 hasn't, return 0*/
+        if(ind1 == 0) return 0;
+
+        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
+
+        int result = 0;
+
+        /* If the characters match, consider two options:
+        either leave one character in s1 and s2 or leave 
+        one character in s1 and continue matching s2*/
+        if(s1[ind1] == s2[ind2]) {
+            int leaveOne = countUtil(s1, s2, ind1 - 1, ind2 - 1, dp);
+            int stay = countUtil(s1, s2, ind1 - 1, ind2, dp);
+
+            result = (leaveOne + stay) % mod;
+        } else {
+            /* If characters don't match, just leave
+            one character in s1 and continue matching s2*/
+            result = countUtil(s1, s2, ind1 - 1, ind2, dp);
+        }
+
+        dp[ind1][ind2] = result;
+        return result;
+    }
+public: 
+    int distinctSubsequences(string s, string t){
+        /*
+        int ls = s.size();
+        int lt = t.size();
+
+        vector<vector<int>> dp(ls + 1, vector<int>(lt + 1, -1));
+
+        return countUtil(s, t, ls, lt, dp);
+        
+        int n = s.size(), m = t.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for(int i = 0; i <= n; i++) dp[i][0] = 1;
+        // for(int i = 1; i <= m; i++) dp[0][i] = 0;
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(s[i - 1] == t[j - 1]) {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % mod;
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n][m];
+        */
+        int n = s.size(), m = t.size();
+        vector<int> prev(m + 1, 0);
+
+        prev[0] = 1;
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = m; j >= 1; j--) {
+                if(s[i - 1] == t[j - 1]) {
+                    prev[j] = (prev[j - 1] + prev[j]) % mod;
+                }
+            }
+        }
+        return prev[m];
+    }
+
+    // Recursion - TC = exponential, SC = O(N + M)
+    // Memoization - TC = O(N * M), SC = O(N * M) + O(N + M)
+    // Tabulation - TC = O(N * M), SC = O(N * M)
+    // Space Optimization - TC = O(N * M), SC = O(M)
+};
+
 
 
 int main() {
