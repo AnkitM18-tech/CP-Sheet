788af4f16fd783398fff41585f53dc2de803af32
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-07-18 13:46:52 +0530

    Edit Distance

--- dynamic-programming/dp-on-strings.cpp
+++ dynamic-programming/dp-on-strings.cpp
@@ -393,6 +393,78 @@ public:
     // Space Optimization - TC = O(N * M), SC = O(M)
 };
 
+class EditDistance {
+private:
+    int editDistanceUtil(string& s1, string& s2, int i, int j, vector<vector<int>>& dp) {
+        // base case
+        if(i < 0) return j + 1;
+        if(j < 0) return i + 1;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(s1[i] == s2[j]) {
+            return dp[i][j] = 0 + editDistanceUtil(s1, s2, i - 1, j - 1, dp);
+        } else {
+            return dp[i][j] = 1 + min(editDistanceUtil(s1, s2, i - 1, j -1, dp), 
+            min(editDistanceUtil(s1, s2, i - 1, j, dp), editDistanceUtil(s1, s2, i, j - 1, dp)));
+        }
+    }
+
+public:
+    int editDistance(string start, string target) {
+        int n = start.size(), m = target.size();
+        // vector<vector<int>> dp(n, vector<int>(m, -1));
+
+        // return editDistanceUtil(start, target, n - 1, m - 1, dp);
+        /*
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for(int i = 0; i <= n; i++) {
+            dp[i][0] = i;
+        }
+
+        for(int j = 0; j <= m; j++) {
+            dp[0][j] = j; 
+        }
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(start[i - 1] == target[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1]));
+                }
+            }
+        }
+
+        return dp[n][m];
+        */
+        vector<int> prev(m + 1, 0), cur(m + 1, 0);
+
+        // Initialize first row
+        for(int j = 0; j <= m; j++) prev[j] = j;
+
+        for(int i = 1; i <= n; i++) {
+            // Initialize first column of current row
+            cur[0] = i;
+            for(int j = 1; j <= m; j++) {
+                if(start[i - 1] == target[j - 1]) {
+                    cur[j] = prev[j - 1];
+                } else {
+                    cur[j] = 1 + min(prev[j - 1], min(prev[j], cur[j - 1]));
+                }
+            }
+            prev = cur;
+        }
+
+        return cur[m];
+    }
+    // max operations - N deletions + M insertions
+    // insert - i, j - 1 | delete - i - 1, j | replace - i - 1, j - 1
+    // base case - if s1 gets exhausted then j + 1 no of insertions still needed
+    // likewise if s2 gets exhausted then i + 1 no of deletions needed
+};
+
 
 
 int main() {
