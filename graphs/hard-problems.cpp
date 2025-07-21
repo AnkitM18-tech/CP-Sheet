c1ef271ecd8039a9357bc87086da38d47863b8fd
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-07-21 18:47:24 +0530

    shortest path in DAG

--- graphs/hard-problems.cpp
+++ graphs/hard-problems.cpp
@@ -268,6 +268,73 @@ public:
     */
 };
 
+class ShortestPathInDAG {
+private:
+    void topoSort(int node, vector<pair<int, int>> adj[], 
+    vector<bool>& vis, stack<int>& st) {
+        vis[node] = 1;
+
+        for(auto it : adj[node]) {
+            int v = it.first;
+
+            if(!vis[v]) {
+                topoSort(v, adj, vis, st);
+            }
+        }
+
+        /* Add the current node to stack 
+        once all the nodes connected to it 
+        have been processed */
+        st.push(node);
+    }
+
+public:
+    vector<int> shortestPath(int N, int M, vector<vector<int>>& edges) {
+        vector<pair<int, int>> adj[N];
+
+        for(int i = 0; i < M; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int wt = edges[i][2];
+
+            adj[u].push_back({v, wt});
+        }
+
+        vector<bool> vis(N, false);
+        stack<int> st;
+
+        for(int i = 0; i < N; i++) {
+            if(!vis[i]) {
+                topoSort(i, adj, vis, st);
+            }
+        }
+
+        vector<int> dist(N, 1e9);
+        // Distance of source node to itself is zero
+        dist[0] = 0;
+
+        while(!st.empty()) {
+            int node = st.top();
+            st.pop();
+
+            for(auto it : adj[node]) {
+                int v = it.first, wt = it.second;
+
+                if(dist[node] + wt < dist[v]) {
+                    dist[v] = wt + dist[node];
+                }
+            }
+        }
+
+        for(int i = 0; i < N; i++) {
+            if(dist[i] == 1e9) dist[i] = -1;
+        }
+
+        return dist;
+    }
+    // TC = SC = O(N + M)
+};
+
 
 
 int main() {
