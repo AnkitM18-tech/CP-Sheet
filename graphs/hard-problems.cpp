5d820e586c9371c2c4f9f1c1c0752d8e348b4690
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-07-22 18:48:49 +0530

    word ladder

--- graphs/hard-problems.cpp
+++ graphs/hard-problems.cpp
@@ -381,6 +381,46 @@ public:
     // TC = SC = O(N + M)
 };
 
+class WordLadderI {
+public:
+    int wordLadderLength(string startWord, string targetWord,vector<string> &wordList) {
+        // Queue data structure to store pair:
+        // {"word", steps to reach "word"}
+        queue<pair<string, int>> q;
+        q.push({startWord, 1});
+
+        unordered_set<string> st(wordList.begin(), wordList.end());
+        // erase starting word from set if it exists
+        st.erase(startWord);
+
+        while(!q.empty()) {
+            string word = q.front().first;
+            int steps = q.front().second;
+            q.pop();
+
+            if(word == targetWord) return steps;
+
+            for(int i = 0; i < word.size(); i++) {
+                char original = word[i];
+                for(char ch = 'a'; ch <= 'z'; ch++) {
+                    word[i] = ch;
+
+                    if(st.find(word) != st.end()) {
+                        st.erase(word);
+                        q.push({word, steps + 1});
+                    }
+                }
+
+                word[i] = original;
+            }
+        }
+
+        return 0;
+    }
+
+    // TC = O(N * M * 26), SC = O(N)
+};
+
 
 
 int main() {
