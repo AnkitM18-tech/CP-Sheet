8a8dc5c8db9f92385fd109cffbdc6a506d00f437
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-07-28 20:03:13 +0530

    Accounts merge

--- graphs/hard-problems.cpp
+++ graphs/hard-problems.cpp
@@ -552,6 +552,116 @@ public:
     // TC = O(N + M), SC = O(N)
 };
 
+class DisjointSet {
+public:
+    /* To store the ranks, parents and 
+    sizes of different set of vertices */
+    vector<int> rank, parent, size;
+
+    // Constructor
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+    
+    // Function to find ultimate parent
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+    
+    // Function to implement union by rank
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+    
+    // Function to implement union by size
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class AccountsMerge{
+public:
+    vector<vector<string>> accountsMerge(vector<vector<string>> accounts){
+        int n = accounts.size();
+
+        DisjointSet ds(n);
+
+        unordered_map<string, int> mapMailNode;
+        
+        // Iterate on all the accounts
+        for(int i = 0; i < n; i++) {
+            // Iterate on all the mails of the person
+            for(int j = 1; j < accounts[i].size(); j++) {
+                string mail = accounts[i][j];
+
+                if(mapMailNode.find(mail) == mapMailNode.end()) {
+                    mapMailNode[mail] = i;
+                } else {
+                    ds.unionBySize(i, mapMailNode[mail]);
+                }
+            }
+        }
+
+        vector<string> mergedMail[n];
+
+        for(auto it : mapMailNode) {
+            string mail = it.first;
+            int node = ds.findUPar(it.second);
+            mergedMail[node].push_back(mail);
+        }
+
+        vector<vector<string>> ans;
+
+        for(int i = 0; i < n; i++) {
+            if(mergedMail[i].size() == 0) continue;
+
+            sort(mergedMail[i].begin(), mergedMail[i].end());
+            vector<string> temp;
+            temp.push_back(accounts[i][0]);
+            for(auto it: mergedMail[i]) {
+                temp.push_back(it);
+            }
+
+            ans.push_back(temp);
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+    // TC = O(N + E) + O(E*4a) + O(N*(ElogE + E)) E = no of emails
+    // SC = O(N + E)
+    
+};
+
 
 
 int main() {
