cd11aa6c0a23429c31c723a235139474c3c3de1e
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2025-07-31 20:50:27 +0530

    N Queen Problem

--- recursion/faq-hard.cpp
+++ recursion/faq-hard.cpp
@@ -36,6 +36,68 @@ public:
     // TC = O(2^N), SC = O(N)
 };
 
+class NQueens {
+public:
+    bool safe(vector<string>& board, int row, int col) {
+        int r = row, c = col;
+
+        // Upper Left Diagonal
+        while(r >= 0 && c >= 0) {
+            if(board[r][c] == 'Q') return false;
+            r--;
+            c--;
+        }
+
+        // Reset to original position
+        r = row, c = col;
+
+        // check left side
+        while(c >= 0) {
+            if(board[r][c] == 'Q') return false;
+            c--;
+        }
+
+        // Reset to the original position
+        r = row, c = col;
+
+        // check lower left diagonal
+        while(r < board.size() && c >= 0) {
+            if(board[r][c] == 'Q') return false;
+            r++;
+            c--;
+        }
+
+        return true;
+    }
+
+    void func(int col, vector<vector<string>>& ans, vector<string>& board) {
+        if(col == board.size()) {
+            ans.push_back(board);
+            return;
+        }
+
+        for(int row = 0; row < board.size(); row++) {
+            if(safe(board, row, col)) {
+                // place queen
+                board[row][col] = 'Q';
+                func(col + 1, ans, board);
+                // remove the queen and backtrack
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n, string(n, '.'));
+
+        func(0, ans, board);
+        return ans;
+    }
+
+    // TC = O(N!), SC = O(N)
+};
+
 
 
 int main() {
